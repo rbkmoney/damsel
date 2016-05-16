29fc95a5c55fb3e1d73db4191e4b1f9741127adc
Igor Savchuk <trollfred@gmail.com>
2016-05-16 18:12:59 +0300

    add cds.thrift

---
+++ proto/cds.thrift
@@ -0,0 +1,25 @@
+typedef binary MasterKeyShare;
+typedef list<MasterKeyShare> MasterKeyShares;
+typedef binary Token;
+typedef binary CardData;
+
+struct UnlockStatus {
+	1: bool unlocked
+	2: i16 more_keys_needed
+}
+
+exception Locked {}
+
+exception NotFound {}
+
+exception KeyringExists {}
+
+service Cds {
+	MasterKeyShares init (1: i16 threshold, 2: i16 num_shares) throws (1: KeyringExists exists)
+	UnlockStatus unlock (1: MasterKeyShare key_share) throws ()
+	void lock () throws ()
+	void rotate () throws (1: Locked locked)
+
+	CardData getCardData (1: Token token) throws (1: NotFound not_found, 2: Locked locked)
+	Token putCardData (1: CardData card_data) throws (1: Locked locked)
+}
