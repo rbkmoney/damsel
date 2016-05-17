e9b9a0863fc3f0a7076cc627a4315126b0f0dfe0
Igor Savchuk <trollfred@gmail.com>
2016-05-17 14:49:16 +0300

    split in two services
    Cds and CdsAdmin

--- proto/cds.thrift
+++ proto/cds.thrift
@@ -16,12 +16,14 @@ exception NotFound {}
 
 exception KeyringExists {}
 
-service Cds {
+service CdsAdmin {
 	MasterKeyShares init (1: i16 threshold, 2: i16 num_shares) throws (1: KeyringExists exists)
 	UnlockStatus unlock (1: MasterKeyShare key_share) throws (1: NoKeyring no_keyring)
 	void lock () throws ()
 	void rotate () throws (1: Locked locked)
+}
 
+service Cds {
 	CardData getCardData (1: Token token) throws (1: NotFound not_found, 2: Locked locked)
 	Token putCardData (1: CardData card_data) throws (1: Locked locked)
 }
