2ed4ea33355589499747c529074cb75c9dcdff70
Igor Savchuk <trollfred@gmail.com>
2016-05-19 18:08:28 +0300

    tiny little fixes

--- proto/cds.thrift
+++ proto/cds.thrift
@@ -1,19 +1,25 @@
+include "base.thrift"
+
 typedef binary MasterKeyShare;
 typedef list<MasterKeyShare> MasterKeyShares;
 typedef string Token
 typedef string Session
 
 enum BankCardPaymentSystem {
-    visa,
+    visa
     mastercard
+}
+
+struct ExpDate {
+    1: required i8 month
+    2: required i16 year
 }
 
 struct CardData {
 	1: required string pan
-	2: required i8 month
-	3: required i8 year
-	4: required string cardholder_name
-	5: required string cvv
+	2: required ExpDate exp_date
+	3: required string cardholder_name
+	4: required string cvv
 }
 
 struct BankCard {
@@ -23,32 +29,32 @@ struct BankCard {
     4: required string masked_pan
 }
 
-struct putCardDataReturn {
+struct PutCardDataResult {
 	1: required BankCard bank_card
 	2: required Session session
 }
 
-struct UnlockStatus {
+union UnlockStatus {
 	1: bool unlocked
 	2: i16 more_keys_needed
 }
+
+exception Invalid {}
 
 exception NoKeyring {}
 
 exception Locked {}
 
-exception NotFound {}
-
 exception KeyringExists {}
 
-service CdsAdmin {
-	MasterKeyShares init (1: i16 threshold, 2: i16 num_shares) throws (1: KeyringExists exists)
-	UnlockStatus unlock (1: MasterKeyShare key_share) throws (1: NoKeyring no_keyring)
-	void lock () throws ()
-	void rotate () throws (1: Locked locked)
+service Keyring {
+	MasterKeyShares Init (1: i16 threshold, 2: i16 num_shares) throws (1: KeyringExists exists)
+	UnlockStatus Unlock (1: MasterKeyShare key_share) throws (1: NoKeyring no_keyring)
+	void Lock () throws ()
+	void Rotate () throws (1: Locked locked)
 }
 
-service Cds {
-	CardData getCardData (1: Token token) throws (1: NotFound not_found, 2: Locked locked)
-	BankCard putCardData (1: CardData card_data) throws (1: Locked locked)
+service Storage {
+	CardData GetCardData (1: Token token) throws (1: base.NotFound not_found, 2: Locked locked)
+	BankCard PutCardData (1: CardData card_data) throws (1: Locked locked, 2: Invalid invalid)
 }
