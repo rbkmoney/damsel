81dbeef917245ea50644da7da4b19cc52092fa02
Igor Savchuk <trollfred@gmail.com>
2016-05-20 13:35:00 +0300

    tabs to spaces

--- proto/cds.thrift
+++ proto/cds.thrift
@@ -16,10 +16,10 @@ struct ExpDate {
 }
 
 struct CardData {
-	1: required string pan
-	2: required ExpDate exp_date
-	3: required string cardholder_name
-	4: required string cvv
+    1: required string pan
+    2: required ExpDate exp_date
+    3: required string cardholder_name
+    4: required string cvv
 }
 
 struct BankCard {
@@ -30,13 +30,13 @@ struct BankCard {
 }
 
 struct PutCardDataResult {
-	1: required BankCard bank_card
-	2: required Session session
+    1: required BankCard bank_card
+    2: required Session session
 }
 
 union UnlockStatus {
-	1: bool unlocked
-	2: i16 more_keys_needed
+    1: bool unlocked
+    2: i16 more_keys_needed
 }
 
 exception Invalid {}
@@ -48,15 +48,15 @@ exception Locked {}
 exception KeyringExists {}
 
 service Keyring {
-	MasterKeyShares Init (1: i16 threshold, 2: i16 num_shares) throws (1: KeyringExists exists)
-	UnlockStatus Unlock (1: MasterKeyShare key_share) throws (1: NoKeyring no_keyring)
-	void Lock () throws ()
-	void Rotate () throws (1: Locked locked)
+    MasterKeyShares Init (1: i16 threshold, 2: i16 num_shares) throws (1: KeyringExists exists)
+    UnlockStatus Unlock (1: MasterKeyShare key_share) throws (1: NoKeyring no_keyring)
+    void Lock () throws ()
+    void Rotate () throws (1: Locked locked)
 }
 
 service Storage {
-	CardData GetCardData (1: Token token) throws (1: base.NotFound not_found, 2: Locked locked)
-	CardData GetSessionCardData (1: Token token, 2: Session session)
-		throws (1: base.NotFound not_found, 2: Locked locked)
-	BankCard PutCardData (1: CardData card_data) throws (1: Locked locked, 2: Invalid invalid)
+    CardData GetCardData (1: Token token) throws (1: base.NotFound not_found, 2: Locked locked)
+    CardData GetSessionCardData (1: Token token, 2: Session session)
+        throws (1: base.NotFound not_found, 2: Locked locked)
+    BankCard PutCardData (1: CardData card_data) throws (1: Locked locked, 2: Invalid invalid)
 }
