06308f6fb3e3f8bb1c61088465ea02834b713483
Igor Savchuk <trollfred@gmail.com>
2016-05-24 16:15:11 +0300

    [CDS-7] add cds.thrift (#9)
    * add cds.thrift
    
    * add NoKeyring exception
    
    * split in two services
    
    Cds and CdsAdmin
    
    * Design a draft of domain data model and config service
    
    HG-5
    HG-6
    
    * HG-7: Add a couple of fixes
    
    * HG-7: Fixate namespaces a bit
    
    * Fix review comments
    
    HG-7
    HG-5
    
    * introduce bankcard and carddata types
    
    * tiny little fixes
    
    * getSessionCardData
    
    * tabs to spaces
    
    * fix result of put
    
    * HG-20: Draft a demoable processing service (#10)
    
    
    
    * HG-20: Fix issues found during review
    
    * cds fixes

---
+++ proto/cds.thrift
@@ -0,0 +1,63 @@
+include "base.thrift"
+
+typedef binary MasterKeyShare;
+typedef list<MasterKeyShare> MasterKeyShares;
+typedef string Token
+typedef string Session
+
+enum BankCardPaymentSystem {
+    visa
+    mastercard
+}
+
+struct ExpDate {
+    1: required i8 month
+    2: required i16 year
+}
+
+struct CardData {
+    1: required string pan
+    2: required ExpDate exp_date
+    3: required string cardholder_name
+    4: required string cvv
+}
+
+struct BankCard {
+    1: required Token token
+    2: required BankCardPaymentSystem payment_system
+    3: required string bin
+    4: required string masked_pan
+}
+
+struct PutCardDataResult {
+    1: required BankCard bank_card
+    2: required Session session
+}
+
+union UnlockStatus {
+    1: base.Ok ok
+    2: i16 more_keys_needed
+}
+
+exception InvalidCardData {}
+
+exception NoKeyring {}
+
+exception KeyringLocked {}
+
+exception KeyringExists {}
+
+service Keyring {
+    MasterKeyShares Init (1: i16 threshold, 2: i16 num_shares) throws (1: KeyringExists exists)
+    UnlockStatus Unlock (1: MasterKeyShare key_share) throws (1: NoKeyring no_keyring)
+    void Lock () throws ()
+    void Rotate () throws (1: KeyringLocked locked)
+}
+
+service Storage {
+    CardData GetCardData (1: Token token) throws (1: base.NotFound not_found, 2: KeyringLocked locked)
+    CardData GetSessionCardData (1: Token token, 2: Session session)
+        throws (1: base.NotFound not_found, 2: KeyringLocked locked)
+    PutCardDataResult PutCardData (1: CardData card_data)
+        throws (1: InvalidCardData invalid, 2: KeyringLocked locked)
+}
