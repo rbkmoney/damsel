16e93ec293c70124220751f343e7ed8734115ec8
Igor Savchuk <trollfred@gmail.com>
2016-06-23 16:20:07 +0300

    rework dmt

--- proto/domain_config.thrift
+++ proto/domain_config.thrift
@@ -11,10 +11,12 @@ namespace erl domain
  * Маркер вершины истории.
  */
 struct Head {}
+
 typedef i64 Version
 
 /**
- * Вариант представления ревизии в истории.
+ * Референс может указывать либо на конкретную
+ * версию либо на наиболее актуальную.
  */
 union Reference {
     1: Version version;
@@ -22,8 +24,23 @@ union Reference {
 }
 
 /**
+ * Снэпшот это определенная версии домена
+ * в терминлологии конфигуратора
+ */
+typedef domain.Domain Snapshot
+
+/**
  * Возможные операции над набором объектов.
  */
+ 
+struct Commit {
+    1: required list<Operation> ops
+}
+
+/**
+ * История это последовательность коммитов
+ */
+typedef map<Version, Commit> History
 
 union Operation {
     1: InsertOp insert;
@@ -35,12 +52,16 @@ struct InsertOp {
     1: required domain.DomainObject object;
 }
 
+/**
+ * Содержит значения до и после внесенных изменений
+ */
 struct UpdateOp {
-    1: required domain.DomainObject object;
+    1: required domain.DomainObject old_object;
+    2: required domain.DomainObject new_object;
 }
 
 struct DeleteOp {
-    1: required domain.Reference ref;
+    1: required domain.DomainObject object;
 }
 
 exception VersionNotFound {}
@@ -51,7 +72,10 @@ exception OperationConflict {}
  * Интерфейс сервиса конфигурации предметной области.
  */
 service Configurator {
-
+    
+    /**
+     * Возвращает объект из домена определенной или последней версии
+     */
     domain.DomainObject checkoutObject (1: Reference version_ref, 2: domain.Reference object_ref)
         throws (1: VersionNotFound ex1, 2: ObjectNotFound ex2);
 
@@ -59,7 +83,24 @@ service Configurator {
 
 service ConfiguratorAdmin {
 
-    Version commit (1: Version v, 2: list<Operation> ops)
+    /**
+     * Применить изменения к определенной версии v.
+     * Возвращает более старшую ближайшую к определенной версию, в которой содержатся
+     * изменения, представленные в коммите c
+     */
+    Version commit (1: Version v, 2: Commit c)
         throws (1: VersionNotFound ex1, 2: OperationConflict ex3);
+        
+    /**
+     * Получить снэпшот конкретной версии
+     */
+    Snapshot checkout (1: Version v)
+        throws (1: VersionNotFound ex1)
+
+    /**
+     * Получить новые коммиты начиная с версии v
+     */
+    History pull (1: Version v)
+        throws (1: VersionNotFound ex1)
 
 }
