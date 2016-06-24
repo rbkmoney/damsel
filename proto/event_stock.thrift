4db050a7d5a60e5fd74efc79ed9e340b3640078c
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2016-06-24 19:52:50 +0300

    MB-21: One more iteration

--- proto/event_stock.thrift
+++ proto/event_stock.thrift
@@ -14,7 +14,7 @@ typedef payment_processing.NoLastEvent NoLastEvent
 * Исходное событие, полученное из HG или другого источника.
 */
 union SourceEvent {
-    1: payment_processing.Event processing_event;
+    1: payment_processing.Event processing_event
 }
 
 /**
@@ -22,49 +22,73 @@ union SourceEvent {
 * source_event - Исходное событие, к которому применяeтся фильтр.
 */
 struct StockEvent {
-    1: required SourceEvent source_event;
+    1: required SourceEvent source_event
+}
+
+/**
+* Граница диапазона EventId.
+*/
+union EventIDBound {
+    1: EventID inclusive
+    2: EventID exclusive
 }
 
 /**
 * Диапазон идентификаторов событий.
-* from_id - с какого ID (inclusive), если не задан, берется текущее минимальное значение.
-* to_id - по какой ID (inclusive), если не задан, берется текущее максимальное значение.
-* Если from и to не заданы, либо from > to - диапазон считается некорректным.
+* from_id - с какого ID.
+* to_id - по какой ID.
+* Если from > to - диапазон считается некорректным.
 */
 struct EventIDRange {
-    1: optional EventID from_id;
-    2: optional EventID to_id;
+    1: required EventIDBound from_id
+    2: required EventIDBound to_id
+}
+
+/**
+* Граница диапазона Timestamp.
+*/
+union EventTimeBound {
+    1: Timestamp inclusive;
+    2: Timestamp exclusive;
 }
 
 /**
 * Диапазон времени создания событий.
-* from_time - начальное время (inclusive), если не задан, берется текущее минимальное значение.
-* to_time - конечное время (inclusive), если не задан, берется текущее максимальное значение.
-* Если from_time и to_time не заданы, либо from > to  - диапазон считается некорректным.
+* from_time - начальное время.
+* to_time - конечное время.
+* Если from > to  - диапазон считается некорректным.
 */
 struct EventTimeRange {
-    1: required Timestamp from_time;
-    2: optional Timestamp to_time;
+    1: required EventIDBound from_time;
+    2: required EventIDBound to_time;
 }
 
 /**
-* Диапазон событий по ID и/или времени создания.
-* Если ни один из диапазонов не задан, считается некорректным.
+* Диапазон событий по ID или времени создания.
 */
-struct EventRange {
-    1: optional EventIDRange id_range;
-    2: optional EventTimeRange time_range;
+union EventRange {
+    1: EventIDRange id_range;
+    2: EventTimeRange time_range;
 }
 
 /**
 * Ограничение выборки событий.
-* event_range - диапазон выборки. Нужно явно задавать, какой диапазон ID и/или времени должен быть отфильтрован.
+* event_range - диапазон выборки. Нужно явно задавать, какой диапазон ID или времени должен быть отфильтрован.
 * limit - максимальный размер выборки, неотрицательное целое число.
 */
 struct EventConstraint {
     1: required EventRange event_range;
-    3: optional i32 limit;
+    3: required i32 limit;
 }
+
+/**
+* Ошибка превышения максимального размера блока данных, доступного для отправки клиенту.
+* limit - текущий максимальный размер блока.
+*/
+exception DatasetTooBig {
+    1: i32 limit;
+}
+
 
 /**
 * Интерфейс BM для клиентов.
@@ -72,16 +96,16 @@ struct EventConstraint {
 service EventRepository {
     /**
     * Возвращает события, удовлетворяющие переданному условию.
-    * Возвращаемый набор данных отсортирован по ID. Количество возвращаемых элементов не превышает 10000 независимо от переданного значения limit.
+    * Возвращаемый набор данных отсортирован по ID.
     * Возвращает ошибку InvalidRequest, если диапазон фильтрации или лимит указан некорректно.
+    * Возвращает ошибку DatasetTooBig, если результирующий блок данных слишком большой.
     */
-    StockEvents getEvents(1: EventConstraint constraint) throws (1: InvalidRequest ex1)
+    StockEvents getEvents(1: EventConstraint constraint) throws (1: InvalidRequest ex1, 2: DatasetTooBig ex2)
 
     /**
-    * Возвращает идентификатор наиболее позднего известного на момент исполнения
-    * запроса события.
+    * Возвращает наиболее позднее известное на момент исполнения запроса событие.
     */
-    EventID getLastEventID () throws (1: NoLastEvent ex1)
+    StockEvent getLastEvent () throws (1: NoLastEvent ex1)
 }
 
 
