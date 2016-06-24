4b89a08cecb5cc284123d4636fdd74bb65d15bfe
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2016-06-24 16:43:36 +0300

    MB-21: Removed event filter

--- proto/event_stock.thrift
+++ proto/event_stock.thrift
@@ -59,64 +59,11 @@ struct EventRange {
 /**
 * Ограничение выборки событий.
 * event_range - диапазон выборки. Нужно явно задавать, какой диапазон ID и/или времени должен быть отфильтрован.
-* event_filter - фильтр событий.
 * limit - максимальный размер выборки, неотрицательное целое число.
 */
 struct EventConstraint {
     1: required EventRange event_range;
-    2: optional EventFilter event_filter;
     3: optional i32 limit;
-}
-
-/**
-* Фильтр событий.
-*/
-struct EventFilter {
-    1: required FilterCondition condition;
-}
-
-/**
-* Условие фильтрации. На данный момент это фильтрация по значениям полей модели.
-*/
-union FilterCondition {
-    1: PathFilterConditionSet condition_set;
-}
-
-/**
-* Набор правил фильтрации.
-* Можно фильтровать по примитивным полям, union-ам и enum-ам.
-*/
-struct PathFilterConditionSet {
-    1: required list<PathFilterCondition> conditions;
-}
-/**
-* Правило фильтрации.
-* field_path - путь к фильтруемому полю в формате 'field.child_field.child_field'.
-* operator - оператор, применяемый к значению поля.
-* values - набор предполагаемых значений. Содержит строковые значения примитивных типов, либо имя искомого union-a или enum-a.
-* Пример: отфильтровать события invoice_status_changed имеющие статус InvoicePaid либо InvoiceFulfilled (в union-е под именами paid и fulfilled соответственно):
-* fieldPath='payload.invoice_event.invoice_status_changed.status'
-* operator=ConditionOperator.IN
-* values=['paid', 'fulfilled']
-*/
-struct PathFilterCondition {
-    1: required string field_path;
-    2: required ConditionOperator operator;
-    3: required list<string> values;
-}
-
-/**
-* Допустимые операторы условия фильтрации.
-*/
-enum ConditionOperator {
-    EQ // =  (some bind value)
-    NE // <> (some bind value)
-    LT // <  (some bind value)
-    LE // <= (some bind value)
-    GT // >  (some bind value)
-    GE // >= (some bind value)
-    IN // in (some bind values)
-    NI // not in
 }
 
 /**
@@ -126,7 +73,7 @@ service EventRepository {
     /**
     * Возвращает события, удовлетворяющие переданному условию.
     * Возвращаемый набор данных отсортирован по ID. Количество возвращаемых элементов не превышает 10000 независимо от переданного значения limit.
-    * Возвращает ошибку InvalidRequest, если диапазон фильтрации, фильтр или лимит указан некорректно.
+    * Возвращает ошибку InvalidRequest, если диапазон фильтрации или лимит указан некорректно.
     */
     StockEvents getEvents(1: EventConstraint constraint) throws (1: InvalidRequest ex1)
 
