59f01ac9ec8188000112b1e0b9cced0af397b90e
Vladimir Pankrashkin <gdm000@gmail.com>
2016-08-08 11:11:42 +0300

    MST-1: magista protocol fixes (#43)
    * MST-1: fixes: changed return types, added total records count (without limit/offset), added new exception
    
    * MST-1: text fixes

--- proto/merch_stat.thrift
+++ proto/merch_stat.thrift
@@ -39,6 +39,10 @@ struct StatCustomer {
     2: required base.Timestamp created_at
 }
 
+
+typedef map<string, string> StatInfo
+typedef base.InvalidRequest InvalidRequest
+
 /**
 * Данные запроса к сервису. Формат и функциональность запроса зависят от DSL.
  * DSL содержит условия выборки, а также id мерчанта, по которому производится выборка.
@@ -47,17 +51,51 @@ struct StatRequest {
     1: required string dsl
 }
 
-typedef map<string, string> StatInfo
-typedef base.InvalidRequest InvalidRequest
+/**
+* Данные ответа сервиса.
+* data - данные, тип зависит от целевой функции.
+* total_count - ожидаемое общее количество данных (т.е. размер всех данных результата, без ограничений по количеству)
+*/
+struct StatResponse {
+    1: required StatResponseData data
+    2: optional i32 total_count
+}
+
+/**
+* Возможные варианты возвращаемых данных
+*/
+union StatResponseData {
+    1: list<StatPayment> payments
+    2: list<StatInvoice> invoices
+    3: list<StatCustomer> customers
+    4: list<StatInfo> records
+}
+
+/**
+* Ошибка превышения максимального размера блока данных, доступного для отправки клиенту.
+* limit - текущий максимальный размер блока.
+*/
+exception DatasetTooBig {
+    1: i32 limit;
+}
 
 service MerchantStatistics {
-    list<StatPayment> GetPayments(1: StatRequest req) throws (1: InvalidRequest ex1)
-    list<StatInvoice> GetInvoices(1: StatRequest req) throws (1: InvalidRequest ex1)
-    list<StatCustomer> GetCustomers(1: StatRequest req) throws (1: InvalidRequest ex1)
+    /**
+    * Возвращает набор данных о платежах
+    */
+    StatResponse GetPayments(1: StatRequest req) throws (1: InvalidRequest ex1, 2: DatasetTooBig ex2)
+    /**
+    *  Возвращает набор данных об инвойсах
+    */
+    StatResponse GetInvoices(1: StatRequest req) throws (1: InvalidRequest ex1, 2: DatasetTooBig ex2)
+    /**
+    * Возвращает набор данных о покупателях
+    */
+    StatResponse GetCustomers(1: StatRequest req) throws (1: InvalidRequest ex1, 2: DatasetTooBig ex2)
 
     /**
-    * Возвращает аггрегированные данные, формат возвращаемых данных зависит от целевой функции, указанной в DSL.
+    * Возвращает аггрегированные данные в виде набора записей, формат возвращаемых данных зависит от целевой функции, указанной в DSL.
     */
-    list<StatInfo> GetStat(1: StatRequest req) throws (1: InvalidRequest ex1)
+    StatResponse GetStatistics(1: StatRequest req) throws (1: InvalidRequest ex1, 2: DatasetTooBig ex2)
 }
 
