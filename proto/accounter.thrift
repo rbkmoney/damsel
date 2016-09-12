b4786a734fc818f5a9e04d81e4e3d5b2933a0704
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2016-09-12 15:04:23 +0300

    SHMW-2: review fixes

--- proto/accounter.thrift
+++ proto/accounter.thrift
@@ -4,8 +4,8 @@ include 'domain.thrift'
 namespace java com.rbkmoney.damsel.accounter
 namespace erlang accounter
 typedef base.ID PlanID
-typedef i64 PostingId
-typedef i64 AccountId
+typedef i64 PostingID
+typedef i64 AccountID
 typedef list<PostingLog> PostingLogs
 
 enum PostingOperation {
@@ -15,62 +15,61 @@ enum PostingOperation {
 }
 
 struct Account {
-1: required AccountId id
-2: required domain.Amount own_amount
-3: required domain.Amount available_amount
-4: required domain.CurrencyRef currency_ref
-5: optional string description
+    1: required AccountID id
+    2: required domain.Amount own_amount
+    3: required domain.Amount available_amount
+    4: required domain.CurrencyRef currency_ref
+    5: optional string description
 }
 
 struct Posting {
-1: required AccountId from_id
-2: required AccountId to_id
-3: required domain.Amount amount
-4: required domain.CurrencyRef currency_ref
-5: required string description
+    1: required AccountID from_id
+    2: required AccountID to_id
+    3: required domain.Amount amount
+    4: required domain.CurrencyRef currency_ref
+    5: required string description
 }
 
 struct PostingPlan {
-1: required PlanID id
-2: required list<Posting> batch
+    1: required PlanID id
+    2: required list<Posting> batch
 }
 
 struct PostingLog {
-1: required PostingId id
-2: required PlanID plan_id
-3: required base.Timestamp created_at
-4: required PostingOperation operation
-5: required Posting posting
+    1: required PostingID id
+    2: required PlanID plan_id
+    3: required base.Timestamp created_at
+    4: required PostingOperation operation
+    5: required Posting posting
 }
 
 struct PostingPlanLog {
-1: required PlanID id
-2: required PostingLogs batch_log
-3: required map<AccountId, Account> affected_accounts
+    1: required PlanID id
+    2: required PostingLogs batch_log
+    3: required map<AccountID, Account> affected_accounts
 }
 
 exception AccountNotFound {
- 1: required AccountId account_id
+    1: required AccountID account_id
  }
 
 exception PlanNotFound {
-1: required PlanID plan_id
+    1: required PlanID plan_id
 }
 
 /**
 * Возникает в случае, если переданы некорректные параметры в одной или нескольких проводках
 */
 exception InvalidPostingParams {
-1: required map<Posting, string> wrong_postings
+    1: required map<Posting, string> wrong_postings
 }
 
 
 service Accounter {
-PostingPlanLog HoldPlan(1: PostingPlan plan) throws (1:InvalidPostingParams e1, 2:base.InvalidRequest e2)
-PostingPlanLog CommitPlan(1: PostingPlan plan) throws (1:InvalidPostingParams e1, 2:base.InvalidRequest e2)
-PostingPlanLog RollbackPlan(1: PostingPlan plan) throws (1:InvalidPostingParams e1, 2:base.InvalidRequest e2)
-PostingLogs GetPostingLogs(1: PlanID id) throws (1: PlanNotFound e1, 2:base.InvalidRequest e2)
-AccountId CreateAccount(1: Account prototype) throws (1:base.InvalidRequest e1)
-Account GetAccountById(1: AccountId id) throws (1:AccountNotFound ex)
-
+    PostingPlanLog HoldPlan(1: PostingPlan plan) throws (1:InvalidPostingParams e1, 2:base.InvalidRequest e2)
+    PostingPlanLog CommitPlan(1: PostingPlan plan) throws (1:InvalidPostingParams e1, 2:base.InvalidRequest e2)
+    PostingPlanLog RollbackPlan(1: PostingPlan plan) throws (1:InvalidPostingParams e1, 2:base.InvalidRequest e2)
+    PostingLogs GetPostingLogs(1: PlanID id) throws (1: PlanNotFound e1, 2:base.InvalidRequest e2)
+    AccountID CreateAccount(1: Account prototype) throws (1:base.InvalidRequest e1)
+    Account GetAccountById(1: AccountID id) throws (1:AccountNotFound ex)
 }
