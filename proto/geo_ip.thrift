e37f732d8a38140d74ce10a4d337b2fd1d98bca4
a.semenkov <sealexey@gmail.com>
2016-09-16 15:43:33 +0300

    + cgange requered and optiona + fix exception names + fix set location response

--- proto/geo_ip.thrift
+++ proto/geo_ip.thrift
@@ -17,42 +17,42 @@ enum Lang{
 
 struct LocationInfo {
     // geoId города
-    1: optional GeoId cityGeoId;
+    1: required GeoId cityGeoId;
     // geoId региона
     2: optional GeoId subdivision1GeoId;
     3: optional GeoId subdivision2GeoId;
     // geoId страны
-    4: optional GeoId countryGeoId;
+    4: required GeoId countryGeoId;
     // Долгота
     5: optional double longitude
     // Широта
     6: optional double latitude;
     // Полное описание локации в json
     // полное описание на сайте https://www.maxmind.com/en/geoip2-city
-    7: string rawResponse;
+    7: required string rawResponse;
 }
 
 struct GeoIdInfo{
-   1: GeoId geonameId;
+   1: required GeoId geonameId;
    2: string localeCode;
    3: string continentCode;
    4: string continentName;
    5: string countryIsoCode;
    6: string countryName;
-   7: string subdivision_1IsoCode;
-   8: string subdivision_1Name;
-   9: string subdivision_2IsoCode;
-   10: string subdivision_2Name;
-   11: string cityName;
-   12: string metroCode;
-   13: string timeZone;
+   7: optional string subdivision_1IsoCode;
+   8: optional string subdivision_1Name;
+   9: optional string subdivision_2IsoCode;
+   10: optional string subdivision_2Name;
+   11: optional string cityName;
+   12: optional string metroCode;
+   13: optional string timeZone;
 }
 
 
 /** Исключение, сигнализирующее о том, что невозможно определить местоположение по указанному IP */
-exception GeoIp2Exception {}
+exception CantDetermineLocation {}
 /** Исключение, сигнализирующее о том, что в базе нет описания для указанных geoId */
-exception GeoIpNotFoundException {
+exception GeoIdNotFound {
     1: list<GeoId> geoIds
 }
 
@@ -63,10 +63,10 @@ service GeoIpService {
     /**
     * Возвращает информацию о предполагаемом местоположении по IP
     **/
-    map <IpAdress,LocationInfo> GetLocation(1:IpAdress ip) throws (1:GeoIp2Exception ex1)
+    map <IpAdress,LocationInfo> GetLocation(1:set<IpAdress> ip) throws (1:CantDetermineLocation ex1)
     /**
      * Возвращает текстовое описание места на указанном языке
      **/
-    map <GeoId,GeoIdInfo> GetLocationInfo(1: set<GeoId> geoIds, 2: Lang lang) throws (1:GeoIpNotFoundException ex1)
+    map <GeoIdInfo> GetLocationInfo(1: set<GeoId> geoIds, 2: Lang lang) throws (1:GeoIdNotFound ex1)
 
 }
