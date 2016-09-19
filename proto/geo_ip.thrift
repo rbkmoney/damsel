3946f4a76193226294d40182a488c5f8a57427e1
a.semenkov <sealexey@gmail.com>
2016-09-19 18:30:13 +0300

    + fix IpAddress + fix subdivisions + fix IDs

--- proto/geo_ip.thrift
+++ proto/geo_ip.thrift
@@ -1,50 +1,53 @@
 include "base.thrift"
+include "domain.thrift"
+
 namespace java com.rbkmoney.damsel.geo_ip
 
 /**
-* IPv4 или IPv6 адрес
-**/
-typedef string IpAdress
-/**
 * Идентификатор места по базе http://www.geonames.org/
 **/
-typedef i32 GeoId
+typedef i32 GeoID
 
 struct LocationInfo {
-    // geoId города
-    1: required GeoId cityGeoId;
-    // geoId региона
-    2: optional GeoId subdivision1GeoId;
-    3: optional GeoId subdivision2GeoId;
-    // geoId страны
-    4: required GeoId countryGeoId;
+    // GeoID города
+    1: required GeoID cityGeoID;
+    // GeoID региона
+    2: optional GeoID subdivision1GeoID;
+    3: optional GeoID subdivision2GeoID;
+    // GeoID страны
+    4: required GeoID countryGeoID;
     // Полное описание локации в json
     // полное описание на сайте https://www.maxmind.com/en/geoip2-city
     7: required string rawResponse;
 }
 
-struct GeoIdInfo{
-   1: required GeoId geonameId;
+struct SubdivisionInfo{
+        // глубина в иерархии. Чем ниже тем цифра выше. Например 1 - Московская область. 2 - Подольский район.
+       1: required i16 level
+       2: optional string subdivisionIsoCode;
+       3: optional string subdivisionName;
+}
+
+// информация о данном GeoID
+struct GeoIDInfo{
+   1: required GeoID geonameId;
    2: string localeCode;
    3: string continentCode;
    4: string continentName;
    5: string countryIsoCode;
    6: string countryName;
-   7: optional string subdivision_1IsoCode;
-   8: optional string subdivision_1Name;
-   9: optional string subdivision_2IsoCode;
-   10: optional string subdivision_2Name;
-   11: optional string cityName;
-   12: optional string metroCode;
-   13: optional string timeZone;
+   7: set<SubdivisionInfo> subdivisions;
+   8: optional string cityName;
+   9: optional string metroCode;
+   10: optional string timeZone;
 }
 
 
 /** Исключение, сигнализирующее о том, что невозможно определить местоположение по указанному IP */
 exception CantDetermineLocation {}
-/** Исключение, сигнализирующее о том, что в базе нет описания для указанных geoId */
-exception GeoIdNotFound {
-    1: list<GeoId> geoIds
+/** Исключение, сигнализирующее о том, что в базе нет описания для указанных GeoID */
+exception GeoIDNotFound {
+    1: list<GeoID> geoIDs
 }
 
 /**
@@ -54,11 +57,11 @@ service GeoIpService {
     /**
     * Возвращает информацию о предполагаемом местоположении по IP
     **/
-    map <IpAdress,LocationInfo> GetLocation(1: set<IpAdress> ip) throws (1: CantDetermineLocation ex1)
+    map <domain.IPAddress,LocationInfo> GetLocation(1: set<domain.IPAddress> ip) throws (1: CantDetermineLocation ex1)
     /**
      * Возвращает текстовое описание места на указанном языке
-     * geoIds - список geo-id по которым нужно получить информацию.
+     * GeoIDs - список geo-id по которым нужно получить информацию.
      * lang - язык ответа. Например: "RU", "ENG"
      **/
-    set <GeoIdInfo> GetLocationInfo ( 1: set<GeoId> geoIds, 2:String lang) throws (1: GeoIdNotFound ex1)
+    set <GeoIDInfo> GetLocationInfo ( 1: set<GeoID> geoIDs, 2:string lang) throws (1: GeoIDNotFound ex1)
 }
