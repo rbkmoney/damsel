f17a1b98930122a1af09201a112ba13b48c20f8f
a.semenkov <sealexey@gmail.com>
2016-09-21 14:47:29 +0300

    + rename to undersocre + change signature

--- proto/geo_ip.thrift
+++ proto/geo_ip.thrift
@@ -10,34 +10,36 @@ typedef i32 GeoID
 
 struct LocationInfo {
     // GeoID города
-    1: required GeoID cityGeoID;
+    1: required GeoID city_geo_id;
     // GeoID страны
-    2: required GeoID countryGeoID;
+    2: required GeoID country_geo_id;
     // Полное описание локации в json
     // подробное описание на сайте https://www.maxmind.com/en/geoip2-city
-    3: required string rawResponse;
+    3: required string raw_response;
 }
 // Информация о регоине
 struct SubdivisionInfo{
         // глубина в иерархии. Чем ниже тем цифра выше. Например 1 - Московская область. 2 - Подольский район.
        1: required i16 level
-       2: optional string subdivisionName;
+       2: optional string subdivision_name;
 }
 
 // Информация о данном GeoID
 struct GeoIDInfo{
-   1: required GeoID geonameId;
-   2: string countryName;
+   1: required GeoID geoname_id;
+   2: string country_name;
    3: set<SubdivisionInfo> subdivisions;
-   4: optional string cityName;
+   4: optional string city_name;
 }
 
 
-/** Исключение, сигнализирующее о том, что невозможно определить местоположение по одному из указанных IP */
-exception CantDetermineLocation {}
+/** Исключение, сигнализирующее о том, что невозможно определить местоположение по IP */
+exception CantDetermineLocation {
+}
 /** Исключение, сигнализирующее о том, что в базе нет описания для одного из указанных GeoID */
 exception LocationNotFound {
-    1: list<GeoID> geoIDs
+    // список идентификаторов данных о которых нет в базе
+    1: list<GeoID> geo_ids
 }
 
 /**
@@ -47,11 +49,11 @@ service GeoIpService {
     /**
     * Возвращает информацию о предполагаемом местоположении по IP
     **/
-    map <domain.IPAddress, LocationInfo> GetLocation(1: set<domain.IPAddress> ip) throws (1: CantDetermineLocation ex1)
+    LocationInfo GetLocation (1: domain.IPAddress ip) throws (1: CantDetermineLocation ex1)
     /**
      * Возвращает текстовое описание места на указанном языке
      * GeoIDs - список geo-id по которым нужно получить информацию.
      * lang - язык ответа. Например: "RU", "ENG"
      **/
-    map <GeoID, GeoIDInfo> GetLocationInfo (1: set<GeoID> geoIDs, 2: string lang) throws (1: LocationNotFound ex1)
+    map <GeoID, GeoIDInfo> GetLocationInfo (1: set<GeoID> geo_ids, 2: string lang) throws (1: LocationNotFound ex1)
 }
