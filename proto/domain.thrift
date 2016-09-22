5b317db5d0eace6b9b1aaa8d0018e7657f03ad50
Andrey Mayorov <a.mayorov@rbkmoney.com>
2016-09-22 20:25:29 +0300

    HG-61: Introduce suspend intent + callbacks, redesign proxy interfaces

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -104,12 +104,14 @@ struct InvoicePayment {
 }
 
 struct InvoicePaymentPending   {}
-struct InvoicePaymentSucceeded {}
+struct InvoicePaymentProcessed {}
+struct InvoicePaymentCaptured  {}
 struct InvoicePaymentFailed    { 1: OperationError err }
 
 union InvoicePaymentStatus {
     1: InvoicePaymentPending pending
-    2: InvoicePaymentSucceeded succeeded
+    4: InvoicePaymentProcessed processed
+    2: InvoicePaymentCaptured captured
     3: InvoicePaymentFailed failed
 }
 
@@ -371,7 +373,7 @@ enum ProxyType {
 struct Proxy {
     1: required ProxyType type
     2: required string url
-    3: optional ProxyOptions options
+    3: optional ProxyOptions options = {}
 }
 
 struct ProxyRef { 1: required ObjectID id }
