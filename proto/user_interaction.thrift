5410e2dcdf119e84acdb415037d9bf1a9cfe17c6
Andrey Mayorov <a.mayorov@rbkmoney.com>
2016-09-23 17:10:14 +0300

    HG-60: Simplify HTTP request significantly

--- proto/user_interaction.thrift
+++ proto/user_interaction.thrift
@@ -3,30 +3,32 @@ include "base.thrift"
 namespace java com.rbkmoney.damsel.user_interaction
 
 /**
- * Строковый шаблон, грамматика согласно [RFC6570](https://tools.ietf.org/html/rfc6570).
+ * Строковый шаблон согласно [RFC6570](https://tools.ietf.org/html/rfc6570) Level 4.
  */
 typedef string Template
 
 /**
- * Набор заголовков HTTP-запроса, каждый из которых может быть шаблонизирован.
- * Набор переменных указан ниже.
+ * Форма, представленная набором полей и их значений в виде строковых шаблонов.
  */
-typedef map<string, Template> HTTPHeaders
+typedef map<string, Template> Form
 
 /**
- * Тело HTTP-запроса, определяющее набор данных и тип содержимого.
+ * Запрос HTTP, пригодный для отправки средствами браузера.
  */
-union HTTPRequestBody {
-    /** Данные формы в виде строкового шаблона, набор переменных указан ниже. */
-    1: Template form
-    2: base.Content arbitrary
+union BrowserHTTPRequest {
+    1: BrowserGetRequest get_request
+    2: BrowserPostRequest post_request
 }
 
-struct HTTPRequest {
+struct BrowserGetRequest {
     /** Шаблон URI запроса, набор переменных указан ниже. */
     1: required Template uri
-    2: required HTTPHeaders headers = []
-    3: optional HTTPRequestBody body
+}
+
+struct BrowserPostRequest {
+    /** Шаблон URI запроса, набор переменных указан ниже. */
+    1: required Template uri
+    2: required Form form
 }
 
 union UserInteraction {
@@ -38,5 +40,5 @@ union UserInteraction {
      *    URI, на который следует переадресовать user agent пользователя по завершении
      *    взаимодействия.
      */
-    1: HTTPRequest redirect
+    1: BrowserHTTPRequest redirect
 }
