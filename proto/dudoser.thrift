4c5378e48904e0278cef94815a2ae7302fbaed64
Inal Arsanukaev <in.all.young@gmail.com>
2016-11-14 18:11:06 +0300

    hotfixes-3

--- proto/dudoser.thrift
+++ proto/dudoser.thrift
@@ -4,15 +4,15 @@ include "domain.thrift"
 namespace java com.rbkmoney.damsel.dudoser
 namespace erlang dudoser
 
-typedef string MailMessageBody
-typedef string MailSubject
-typedef string MailTo
-typedef list<list<byte>> MailAttachments
+typedef string MessageBody
+typedef string Subject
+typedef string To
+typedef list<list<byte>> Attachments
 
 
 
 //Простой идентификатор шаблона
-typedef i32 MailTemplateIDSimple
+typedef i32 TemplateIDSimple
 
 /**
 * Составной идентификатор шаблона письма
@@ -20,7 +20,7 @@ typedef i32 MailTemplateIDSimple
 * merchantID - идентификатор мерчанта
 * shopID - идентификатор магазина
 */
-struct MailTemplateIDComplex {
+struct TemplateIDComplex {
     1: string messageType
     2: i32 merchantID
     3: domain.ShopID shopID
@@ -29,30 +29,30 @@ struct MailTemplateIDComplex {
 /**
 * Юнион из двух видов идентификаторов
 **/
-union MailTemplateID {
-    1: MailTemplateIDComplex mailTemplateIDForShop
-    2: MailTemplateIDSimple mailTemplateID
+union TemplateID {
+    1: TemplateIDComplex templateIDForShop
+    2: TemplateIDSimple templateID
 }
 
-typedef string MailTemplateBody
+typedef string TemplateBody
 
 /**
 * Ошибки, возникающие при отправке письма.
 * Например, недоступен почтовый сервер
 **/
-exception MailSendException {
+exception NotSend {
 }
 
 /**
 * Ошибка, если по указанным идентификаторам не найден шаблон
 **/
-exception MailTemplateNotFoundException {
+exception TemplateNotFound {
 }
 
 /**
 * Ошибка, если пытаются отправить слишком длинное письмо или шаблон
 **/
-exception MailDataTooBigException {
+exception DataTooBig {
     1: i32 limit;
 }
 
@@ -61,47 +61,47 @@ service DudoserService {
     * Отправка готового письма.
     * Параметры - тело, тема письма, адрес, вложения
     **/
-    bool sendMail(1:MailMessageBody message,
-                    2:MailSubject subject,
-                    3:MailTo to,
-                    4:MailAttachments attachments) throws (1: base.InvalidRequest ex1, 2:MailSendException ex2, 3:MailDataTooBigException ex3)
+    bool send(1:MessageBody message,
+                    2:Subject subject,
+                    3:To to,
+                    4:Attachments attachments) throws (1: base.InvalidRequest ex1, 2:NotSend ex2, 3:DataTooBig ex3)
 
     /**
     * Отправка письма по шаблону.
     * Параметры - данные для подставновки в шаблон, идентификатор шаблона, адрес, вложения
     **/
-    void sendMailByTemplate(1:base.StringMap parameters,
-                        2:MailTemplateID templateId,
-                        3:MailTo to,
-                        4:MailAttachments attachments) throws (1: base.InvalidRequest ex1, 2:MailSendException ex2, 3:MailDataTooBigException ex3, 4:MailTemplateNotFoundException ex4)
+    void sendByTemplate(1:base.StringMap parameters,
+                        2:TemplateID templateId,
+                        3:To to,
+                        4:Attachments attachments) throws (1: base.InvalidRequest ex1, 2:NotSend ex2, 3:DataTooBig ex3, 4:TemplateNotFound ex4)
 
     /**
     * Добавление шаблона без привязки к магазину.
     * Параметры - тело шаблона (возможно параметризованное), вложения
     * Результат - возвращается числовой идентификатор шаблона
     **/
-    MailTemplateIDSimple addTemplate(1:MailTemplateBody template,
-                        2:MailAttachments attachments) throws (1: base.InvalidRequest ex1, 2:MailDataTooBigException ex2)
+    TemplateIDSimple addTemplate(1:TemplateBody template,
+                        2:Attachments attachments) throws (1: base.InvalidRequest ex1, 2:DataTooBig ex2)
 
     /**
     * Добавление шаблона с привязкой к магазину.
     * Параметры - тело шаблона, комплексный идентификатор шаблона, описывающий привязку шаблона к магазину, вложения
     * Результат - возвращается числовой идентификатор шаблона
     **/
-    MailTemplateIDSimple addTemplateForShop(1:MailTemplateBody template,
-                            2:MailTemplateID mailTemplateIDForShop,
-                            3:MailAttachments attachments) throws (1: base.InvalidRequest ex1, 2:MailDataTooBigException ex2)
+    TemplateIDSimple addTemplateForShop(1:TemplateBody template,
+                            2:TemplateID templateIDForShop,
+                            3:Attachments attachments) throws (1: base.InvalidRequest ex1, 2:DataTooBig ex2)
 
     /**
     * Редактирование существующего шаблона.
     * Параметры - общий идентификатор шаблона, тело шаблона, вложения
     **/
-    void updateTemplate(1:MailTemplateID templateId,
-                        2:MailTemplateBody template,
-                        3:MailAttachments attachments) throws (1: base.InvalidRequest ex1, 2:MailDataTooBigException ex2, 3:MailTemplateNotFoundException ex3)
+    void updateTemplate(1:TemplateID templateId,
+                        2:TemplateBody template,
+                        3:Attachments attachments) throws (1: base.InvalidRequest ex1, 2:DataTooBig ex2, 3:TemplateNotFound ex3)
     /**
     * Удаление шаблона.
     **/
-    void removeTemplate(1:MailTemplateID templateId) throws (1: base.InvalidRequest ex1, 2:MailTemplateNotFoundException ex2)
+    void removeTemplate(1:TemplateID templateId) throws (1: base.InvalidRequest ex1, 2:TemplateNotFound ex2)
 
 }
