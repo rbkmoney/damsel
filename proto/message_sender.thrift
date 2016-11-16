4f766bc8675c1856a7a2110e0ea0856e6032ebc0
Inal Arsanukaev <in.all.young@gmail.com>
2016-11-16 17:42:27 +0300

    Fixed after comments-15

--- proto/message_sender.thrift
+++ proto/message_sender.thrift
@@ -36,23 +36,9 @@ struct MessageMail {
     5: optional MessageAttachments attachments
 }
 
-/**
-* Ошибки, возникающие при отправке сообщения во внешнюю систему.
-* Например, недоступен почтовый сервер
-**/
-exception MessageNotSend {
-}
-
-/**
-* Ошибка, если пытаются отправить слишком длинное сообщение
-**/
-exception MessageDataTooBig {
-    1: i32 limit
-}
-
 service MessageSender {
     /**
     * Отправка сообщения.
     **/
-    void send(1: Message message) throws (1: base.InvalidRequest ex1, 2: MessageNotSend ex2, 3: MessageDataTooBig ex3)
+    void send(1: Message message) throws (1: base.InvalidRequest ex1)
 }
