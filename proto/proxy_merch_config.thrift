462ea5430a7a682d0253e40164afbe83971d9f9a
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2016-12-13 16:32:43 +0300

    PROX-25: added exception for createOptions

--- proto/proxy_merch_config.thrift
+++ proto/proxy_merch_config.thrift
@@ -14,6 +14,6 @@ struct MerchantProxyConfiguration {
 }
 
 service ConfigureMerchantProxy {
-    domain.ProxyOptions createOptions(1: MerchantProxyParams merchant_proxy_params)
+    domain.ProxyOptions createOptions(1: MerchantProxyParams merchant_proxy_params) throws (1: base.InvalidRequest ex1)
     MerchantProxyConfiguration renderOptions(1: domain.ProxyOptions proxy_options)
 }
