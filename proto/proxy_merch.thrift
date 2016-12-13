ba16cbc4c50629cb8d0b4d0a7f2fc3b8a6a02784
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2016-12-13 16:28:39 +0300

    PROX-25: removed unused struct

--- proto/proxy_merch.thrift
+++ proto/proxy_merch.thrift
@@ -4,11 +4,6 @@ include "proxy_provider.thrift"
 namespace java com.rbkmoney.damsel.proxy_merch
 namespace erlang proxy_merch
 
-struct ShopOptions {
-    1: required string callback_url
-    2: required string priv_key
-}
-
 service MerchantProxy {
     void sendCallback(1: domain.ProxyOptions proxyOptions, 2: proxy_provider.Context context)
 }
