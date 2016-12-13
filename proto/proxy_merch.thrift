0c9ef6f78fc9d9c1b69929d01a66d1f1205cd8f4
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2016-12-13 16:27:47 +0300

    PROX-25: renamed method, added context param to sendCallback method

--- proto/proxy_merch.thrift
+++ proto/proxy_merch.thrift
@@ -1,8 +1,14 @@
 include "domain.thrift"
+include "proxy_provider.thrift"
 
 namespace java com.rbkmoney.damsel.proxy_merch
 namespace erlang proxy_merch
 
+struct ShopOptions {
+    1: required string callback_url
+    2: required string priv_key
+}
+
 service MerchantProxy {
-    void confirmPayment(1: domain.ProxyOptions options)
+    void sendCallback(1: domain.ProxyOptions proxyOptions, 2: proxy_provider.Context context)
 }
