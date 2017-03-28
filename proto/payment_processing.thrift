b53a2cc90e77658a23126a7df92e11c163a084b8
Evgeny Levenets <levenets.e.e@gmail.com>
2017-03-28 13:54:17 +0300

    Category is back to ShopParams as optional field

--- proto/payment_processing.thrift
+++ proto/payment_processing.thrift
@@ -348,6 +348,7 @@ struct PayoutToolParams {
 }
 
 struct ShopParams {
+    1: optional domain.CategoryRef category
     6: required domain.ShopLocation location
     2: required domain.ShopDetails details
     3: required domain.ContractID contract_id
