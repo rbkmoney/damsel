8ae7f6b93c9894ba129f88353ffe07c700ce80fb
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2017-03-31 16:23:11 +0300

    microfix2

--- proto/webhooker.thrift
+++ proto/webhooker.thrift
@@ -11,18 +11,18 @@ exception WebhookNotFound {}
 struct Webhook {
     1: required base.ID id
     2: required domain.PartyID party_id
-    3: required FilterStruct filter_struct
+    3: required EventFilter event_filter
     4: required Url url
     5: required Key pub_key
 }
 
 struct WebhookParams {
     1: required domain.PartyID party_id
-    2: required FilterStruct filter_struct
+    2: required EventFilter filter_struct
     3: required Url url
 }
 
-union FilterStruct {
+union EventFilter {
     1: InvoiceCreated invoice_created
     2: InvoicePaid invoice_paid
     3: PartyShopCreated party_shop_created
@@ -40,7 +40,7 @@ struct PartyShopCreated {
 }
 
 service WebhookManager {
-    list<Webhook> GetWebhooks(1: domain.PartyID party_id)
-    Webhook CreateHook(1: WebhookParams web_hook_params)
-    void DeleteHook(1: base.ID web_hook_id) throws (1: WebhookNotFound ex1)
+    list<Webhook> Get(1: domain.PartyID party_id)
+    Webhook Create(1: WebhookParams webhook_params)
+    void Delete(1: base.ID webhook_id) throws (1: WebhookNotFound ex1)
 }
