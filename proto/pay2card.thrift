efac539cfa9b651de51ed492d67535467e7ffc07
Yevgeniy Poluektov <evgeniy.v.poluektov@gmail.com>
2017-06-16 17:27:40 +0300

    add pay2card interface

---
+++ proto/pay2card.thrift
@@ -0,0 +1,21 @@
+include "base.thrift"
+include "domain.thrift"
+
+namespace java com.rbkmoney.damsel.pay2card
+namespace erlang prxpayout
+
+
+struct Cash {
+    1: required domain.Amount amount
+    2: required domain.Currency currency
+}
+
+ /** Неуспешное завершение взаимодействия с пояснением возникшей проблемы. */
+exception Failure {
+    1: required string code
+    2: optional string description
+}
+
+service Pay2CardService {
+    void makePay (1: domain.Token cardToken, 2: Cash cash) throws (1: Failure ex1)
+}
