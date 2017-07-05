0150a943ee4b82bf0012759b16a649ecfbdf156c
Natalia Pulina <pulitta11@gmail.com>
2017-07-05 17:31:27 +0400

    HG-233: Support of holds added (#159)

--- proto/payment_processing.thrift
+++ proto/payment_processing.thrift
@@ -339,6 +339,18 @@ struct InvoiceTemplateUpdateParams {
 
 struct InvoicePaymentParams {
     1: required domain.Payer payer
+    2: required InvoicePaymentParamsFlow flow
+}
+
+union InvoicePaymentParamsFlow {
+    1: InvoicePaymentParamsFlowInstant instant
+    2: InvoicePaymentParamsFlowHold hold
+}
+
+struct InvoicePaymentParamsFlowInstant   {}
+
+struct InvoicePaymentParamsFlowHold {
+    1: required domain.OnHoldExpiration on_hold_expiration
 }
 
 struct Invoice {
@@ -471,6 +483,33 @@ service Invoicing {
             3: InvoicePaymentNotFound ex3
         )
 
+    void CancelPayment (
+        1: UserInfo user,
+        2: domain.InvoiceID id,
+        3: domain.InvoicePaymentID payment_id
+        4: string reason
+    )
+        throws (
+            1: InvalidUser ex1,
+            2: InvoiceNotFound ex2,
+            3: InvoicePaymentNotFound ex3,
+            4: InvalidPaymentStatus ex4,
+            5: base.InvalidRequest ex5,
+        )
+
+    void CapturePayment (
+        1: UserInfo user,
+        2: domain.InvoiceID id,
+        3: domain.InvoicePaymentID payment_id
+        4: string reason
+    )
+        throws (
+            1: InvalidUser ex1,
+            2: InvoiceNotFound ex2,
+            3: InvoicePaymentNotFound ex3,
+            4: InvalidPaymentStatus ex4,
+            5: base.InvalidRequest ex5,
+        )
     /**
      * Создать поправку к платежу.
      *
