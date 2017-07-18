5bb0dac51ff9d7dd666039d5edf302d69b9d5260
Pavel Popov <popoffpavel@yandex.ru>
2017-07-18 11:52:17 +0300

    BJ-193: Add docs and new methods

--- proto/reports.thrift
+++ proto/reports.thrift
@@ -10,6 +10,7 @@ typedef base.ID ReportID
 typedef base.ID FileID
 typedef domain.PartyID PartyID
 typedef domain.ShopID ShopID
+typedef string URL
 
 /**
 * Ошибка превышения максимального размера блока данных, доступного для отправки клиенту.
@@ -36,6 +37,13 @@ struct ReportRequest {
     3: required ReportTimeRange time_range;
 }
 
+/**
+* report_id - уникальный идентификатор отчета
+* from_time, to_time - за какой период данный отчет
+* report_type - тип отчета TODO стоит ли вывести типы отчетов в протокол?
+* report_file_meta - данные по файлу отчета
+* sign_file_meta - данные по файлу подписи
+*/
 struct Report {
     1: required ReportID report_id;
     2: required Timestamp from_time;
@@ -45,6 +53,12 @@ struct Report {
     6: optional FileMeta sign_file_meta;
 }
 
+/**
+* Данные по файлу
+* file_id - уникальный идентификатор файла
+* bucket_name - id корзины в ceph TODO нужно ли?
+* md5 - md5 содержимого файла TODO нужно ли тоже?
+*/
 struct FileMeta {
     1: required FileID file_id;
     2: required string bucket_name;
@@ -53,8 +67,27 @@ struct FileMeta {
 
 service Reports {
 
+  /**
+  * Получить список отчетов по магазину за указанный промежуток времени
+  */
   list<Report> GetReports(1: ReportRequest request) throws (1: InvalidRequest ex1, 2: DatasetTooBig ex2)
 
+  /**
+  * Сгенерировать акт об оказании услуг по магазину за указанный промежуток времени TODO act of acceptance of services?
+  */
+  void GenerateProvisionOfServiceReport(1: ReportRequest request) throws (1: InvalidRequest ex1)
+
+  /**
+  * Перегенерировать ранее созданный отчет
+  * report_id - идентификатор отчета
+  */
   void RegenerateReport(1: ReportID report_id) throws (1: InvalidRequest ex1)
+
+  /**
+  * Сгенерировать ссылку на файл
+  * file_id - идентификатор файла
+  * expiration - время до которого ссылка будет считаться действительной
+  */
+  URL GeneratePresignedUrl(1: FileID file_id, 2: Timestamp expiration) throws (1: InvalidRequest ex1)
 
 }
