a05133749ec658d0aae429563fd15102814a904e
Yevgeniy Poluektov <evgeniy.v.poluektov@gmail.com>
2017-07-26 11:40:59 +0300

    struct args

--- proto/pay2card.thrift
+++ proto/pay2card.thrift
@@ -21,6 +21,13 @@ exception ProviderFailure {
     3: optional string description
 }
 
+struct TransferRequest {
+    1: required string request_id
+    2: required string actor_id
+    3: required domain.Token card_token
+    4: required Cash cash
+}
+
 struct TransferResult {
     1: required Provider provider
     2: required string transaction_id
@@ -33,5 +40,5 @@ service Pay2Card {
 
     /** requestId - должен состоять только из цифр */
     /** actor_id - идентификатор инициатора перевода, например MerchantId.ShopId */
-    TransferResult makeTransfer (1: string request_id, 2: string actor_id, 3: domain.Token card_token, 4: Cash cash) throws (1: ProviderFailure ex1, 2:  base.InvalidRequest ex2)
+    TransferResult makeTransfer (TransferRequest request) throws (1: ProviderFailure ex1, 2:  base.InvalidRequest ex2)
 }
