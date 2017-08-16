b78f62724f5a92cbfe4881ba53e8a6eda830f781
pulitta <pulitta11@gmail.com>
2017-08-16 16:24:22 +0300

    HG-249: SetCurrent removed

--- proto/sequences.thrift
+++ proto/sequences.thrift
@@ -19,11 +19,6 @@ service Sequences {
     Value GetCurrent (1: SequenceId sequence_id)
 
     /**
-     * Установить текущее значение счетчика
-     */
-    void SetCurrent (1: SequenceId sequence_id, 2: Value current_value, 3: Value max_value)
-
-    /**
      * Получить следующее значение счетчика
      */
     Value GetNext (1: SequenceId sequence_id, 2: Value current_value, 3: Value max_value)
