9632744209438c7c35d998f07b45baf0d74a9776
Evgeny Levenets <levenets.e.e@gmail.com>
2017-08-30 18:13:52 +0300

    DC-51: penetrate invoice cart into invoice template (#212)

--- proto/payment_processing.thrift
+++ proto/payment_processing.thrift
@@ -324,16 +324,18 @@ struct InvoiceWithTemplateParams {
 struct InvoiceTemplateCreateParams {
     1: required PartyID party_id
     2: required ShopID shop_id
-    3: required domain.InvoiceDetails details
     4: required domain.LifetimeInterval invoice_lifetime
-    5: required domain.InvoiceTemplateCost cost
+    7: required string product # for backward compatibility
+    8: optional string description
+    9: required domain.InvoiceTemplateDetails details
     6: required domain.InvoiceContext context
 }
 
 struct InvoiceTemplateUpdateParams {
-    1: optional domain.InvoiceDetails details
     2: optional domain.LifetimeInterval invoice_lifetime
-    3: optional domain.InvoiceTemplateCost cost
+    5: optional string product # for backward compatibility
+    6: optional string description
+    7: optional domain.InvoiceTemplateDetails details
     4: optional domain.InvoiceContext context
 }
 
@@ -631,6 +633,7 @@ service InvoiceTemplating {
             5: InvalidShopStatus ex5,
             6: base.InvalidRequest ex6
         )
+
     void Delete (1: UserInfo user, 2: domain.InvoiceTemplateID id)
         throws (
             1: InvalidUser ex1,
