10d1ab87a103e6be798888c3693243da775513a2
Anton Belyaev <arentrue@users.noreply.github.com>
2017-08-31 15:15:53 +0300

    Align fields nameing between merch_stat and domain (#214)

--- proto/merch_stat.thrift
+++ proto/merch_stat.thrift
@@ -150,8 +150,8 @@ struct StatPayout {
 }
 
 union PayoutType {
-    1: PayoutCard card
-    2: PayoutAccount account
+    1: PayoutCard bank_card
+    2: PayoutAccount bank_account
 }
 
 struct PayoutCard {
