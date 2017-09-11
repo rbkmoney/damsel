d73370c403e61282a87d64222fc0497eb10b49e3
Andrey Mayorov <a.mayorov@rbkmoney.com>
2017-09-11 17:58:33 +0300

    HG-262: Use unallocated tag, well, again

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -959,7 +959,7 @@ struct Terminal {
     2: required string description
     9: optional ProxyOptions options
     10: required RiskScore risk_coverage
-    11: optional PaymentsProvisionTerms terms
+    12: optional PaymentsProvisionTerms terms
 }
 
 union TerminalSelector {
