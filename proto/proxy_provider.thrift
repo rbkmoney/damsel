478235a1bdf49581d833fe30753ca422f5591164
Dmitry Manik <manikdv@users.noreply.github.com>
2017-10-23 15:00:48 +0300

    HG-231: Introduce subscriptions (#163) (#205)

--- proto/proxy_provider.thrift
+++ proto/proxy_provider.thrift
@@ -5,6 +5,69 @@ include "domain.thrift"
 namespace java com.rbkmoney.damsel.proxy_provider
 namespace erlang prxprv
 
+struct RecurrentPaymentTool {
+    1: required base.ID                          id
+    2: required base.Timestamp                   created_at
+    3: required domain.DisposablePaymentResource payment_resource
+    4: required Cash                             minimal_payment_cost
+}
+
+/**
+ * Данные, необходимые для генерации многоразового токена
+ */
+struct RecurrentTokenInfo {
+    1: required RecurrentPaymentTool   payment_tool
+    2: optional domain.TransactionInfo trx
+}
+
+/**
+ * Данные сессии взаимодействия с провайдерским прокси в рамках генерации многоразового токена.
+ */
+struct RecurrentTokenSession {
+    1: optional proxy.ProxyState state
+}
+
+/**
+ * Набор данных для взаимодействия с провайдерским прокси в рамках проведения генерации
+ * многоразового токена.
+ */
+struct RecurrentTokenContext {
+    1: required RecurrentTokenSession session
+    2: required RecurrentTokenInfo    token_info
+    3: optional domain.ProxyOptions   options = {}
+}
+
+struct RecurrentTokenProxyResult {
+    1: required RecurrentTokenIntent   intent
+    2: optional proxy.ProxyState       next_state
+    3: optional domain.Token           token
+    4: optional domain.TransactionInfo trx
+}
+
+union RecurrentTokenIntent {
+    1: RecurrentTokenFinishIntent finish
+    2: proxy.SleepIntent          sleep
+    3: proxy.SuspendIntent        suspend
+}
+
+struct RecurrentTokenFinishIntent {
+    1: required RecurrentTokenFinishStatus status
+}
+
+union RecurrentTokenFinishStatus {
+    1: RecurrentTokenSuccess success
+    2: proxy.Failure         failure
+}
+
+struct RecurrentTokenSuccess {
+    1: required domain.Token token
+}
+
+struct RecurrentTokenCallbackResult {
+    1: required proxy.CallbackResponse    response
+    2: required RecurrentTokenProxyResult result
+}
+
 /**
  * Данные платежа, необходимые для обращения к провайдеру.
  */
@@ -30,12 +93,24 @@ struct Invoice {
     6: required Cash cost
 }
 
+union PaymentResource {
+    1: domain.DisposablePaymentResource disposable_payment_resource
+    2: RecurrentPaymentResource         recurrent_payment_resource
+}
+
+struct RecurrentPaymentResource {
+    1: required domain.PaymentTool      payment_tool
+    2: required domain.Token            rec_token
+}
+
 struct InvoicePayment {
     1: required domain.InvoicePaymentID id
     2: required base.Timestamp created_at
     3: optional domain.TransactionInfo trx
-    4: required domain.Payer payer
+    4: required domain.LegacyPayerDetails payer_details
+    6: required PaymentResource payment_resource
     5: required Cash cost
+    7: required domain.ContactInfo contact_info
 }
 
 struct InvoicePaymentRefund {
@@ -61,9 +136,9 @@ struct Session {
 }
 
 /**
- * Набор данных для взаимодействия с провайдерским прокси.
+ * Набор данных для взаимодействия с провайдерским прокси в рамках платежа.
  */
-struct Context {
+struct PaymentContext {
     1: required Session session
     2: required PaymentInfo payment_info
     3: optional domain.ProxyOptions options = {}
@@ -86,7 +161,7 @@ struct Context {
  *  - идентификатор связанной транзакции _не может измениться_ при последующих обращениях в прокси
  *    по текущему платежу.
  */
-struct ProxyResult {
+struct PaymentProxyResult {
     1: required proxy.Intent intent
     2: optional proxy.ProxyState next_state
     3: optional domain.TransactionInfo trx
@@ -95,12 +170,12 @@ struct ProxyResult {
 /**
  * Результат обработки провайдерским прокси обратного вызова в рамках сессии.
  */
-struct CallbackResult {
+struct PaymentCallbackResult {
     1: required proxy.CallbackResponse response
-    2: required CallbackProxyResult result
+    2: required PaymentCallbackProxyResult result
 }
 
-struct CallbackProxyResult {
+struct PaymentCallbackProxyResult {
     // TODO temporary crutch, remove it as soon as possible
     // An `undefined` means that the suspend will be kept untouched
     1: optional proxy.Intent intent
@@ -111,14 +186,30 @@ struct CallbackProxyResult {
 service ProviderProxy {
 
     /**
-     * Запрос к прокси на проведение взаимодействия с провайдером в рамках сессии.
+     * Запрос к прокси на создание многоразового токена
      */
-    ProxyResult ProcessPayment (1: Context context)
+    RecurrentTokenProxyResult GenerateToken (
+        1: RecurrentTokenContext context
+    )
 
     /**
-     * Запрос к прокси на обработку обратного вызова от провайдера в рамках сессии.
+     * Запрос к прокси на обработку обратного вызова от провайдера в рамках сессии получения
+     * многоразового токена.
      */
-    CallbackResult HandlePaymentCallback (1: proxy.Callback callback, 2: Context context)
+    RecurrentTokenCallbackResult HandleRecurrentTokenCallback (
+        1: proxy.Callback        callback
+        2: RecurrentTokenContext context
+    )
+
+    /**
+     * Запрос к прокси на проведение взаимодействия с провайдером в рамках платежной сессии.
+     */
+    PaymentProxyResult ProcessPayment (1: PaymentContext context)
+
+    /**
+     * Запрос к прокси на обработку обратного вызова от провайдера в рамках платежной сессии.
+     */
+    PaymentCallbackResult HandlePaymentCallback (1: proxy.Callback callback, 2: PaymentContext context)
 
 }
 
@@ -127,9 +218,17 @@ exception PaymentNotFound {}
 service ProviderProxyHost {
 
     /**
-     * Запрос к процессингу на обработку обратного вызова от провайдера.
+     * Запрос к процессингу на обработку обратного вызова от провайдера
+     * в рамках взаимодействия по платежу.
      */
-    proxy.CallbackResponse ProcessCallback (1: proxy.CallbackTag tag, 2: proxy.Callback callback)
+    proxy.CallbackResponse ProcessPaymentCallback (1: proxy.CallbackTag tag, 2: proxy.Callback callback)
+        throws (1: base.InvalidRequest ex1)
+
+    /**
+     * Запрос к процессингу на обработку обратного вызова от провайдера
+     * в рамках взаимодействия по получению многоразового токена.
+     */
+    proxy.CallbackResponse ProcessRecurrentTokenCallback (1: proxy.CallbackTag tag, 2: proxy.Callback callback)
         throws (1: base.InvalidRequest ex1)
 
     /**
