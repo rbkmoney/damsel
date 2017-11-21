35435dd3acd9a3fe1453f5b9816d7723ef9575d2
Evgeny Levenets <levenets.e.e@gmail.com>
2017-11-21 18:10:35 +0300

    remove commas from enums

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -670,256 +670,256 @@ struct CategoryDecision {
 // https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 
 enum Residence {
-    ABH, /*Abkhazia*/
-    AUS, /*Australia*/
-    AUT, /*Austria*/
-    AZE, /*Azerbaijan*/
-    ALB, /*Albania*/
-    DZA, /*Algeria*/
-    ASM, /*American Samoa*/
-    AIA, /*Anguilla*/
-    AGO, /*Angola*/
-    AND, /*Andorra*/
-    ATA, /*Antarctica*/
-    ATG, /*Antigua and Barbuda*/
-    ARG, /*Argentina*/
-    ARM, /*Armenia*/
-    ABW, /*Aruba*/
-    AFG, /*Afghanistan*/
-    BHS, /*Bahamas*/
-    BGD, /*Bangladesh*/
-    BRB, /*Barbados*/
-    BHR, /*Bahrain*/
-    BLR, /*Belarus*/
-    BLZ, /*Belize*/
-    BEL, /*Belgium*/
-    BEN, /*Benin*/
-    BMU, /*Bermuda*/
-    BGR, /*Bulgaria*/
-    BOL, /*Bolivia, plurinational state of*/
-    BES, /*Bonaire, Sint Eustatius and Saba*/
-    BIH, /*Bosnia and Herzegovina*/
-    BWA, /*Botswana*/
-    BRA, /*Brazil*/
-    IOT, /*British Indian Ocean Territory*/
-    BRN, /*Brunei Darussalam*/
-    BFA, /*Burkina Faso*/
-    BDI, /*Burundi*/
-    BTN, /*Bhutan*/
-    VUT, /*Vanuatu*/
-    HUN, /*Hungary*/
-    VEN, /*Venezuela*/
-    VGB, /*Virgin Islands, British*/
-    VIR, /*Virgin Islands, U.S.*/
-    VNM, /*Vietnam*/
-    GAB, /*Gabon*/
-    HTI, /*Haiti*/
-    GUY, /*Guyana*/
-    GMB, /*Gambia*/
-    GHA, /*Ghana*/
-    GLP, /*Guadeloupe*/
-    GTM, /*Guatemala*/
-    GIN, /*Guinea*/
-    GNB, /*Guinea-Bissau*/
-    DEU, /*Germany*/
-    GGY, /*Guernsey*/
-    GIB, /*Gibraltar*/
-    HND, /*Honduras*/
-    HKG, /*Hong Kong*/
-    GRD, /*Grenada*/
-    GRL, /*Greenland*/
-    GRC, /*Greece*/
-    GEO, /*Georgia*/
-    GUM, /*Guam*/
-    DNK, /*Denmark*/
-    JEY, /*Jersey*/
-    DJI, /*Djibouti*/
-    DMA, /*Dominica*/
-    DOM, /*Dominican Republic*/
-    EGY, /*Egypt*/
-    ZMB, /*Zambia*/
-    ESH, /*Western Sahara*/
-    ZWE, /*Zimbabwe*/
-    ISR, /*Israel*/
-    IND, /*India*/
-    IDN, /*Indonesia*/
-    JOR, /*Jordan*/
-    IRQ, /*Iraq*/
-    IRN, /*Iran, Islamic Republic of*/
-    IRL, /*Ireland*/
-    ISL, /*Iceland*/
-    ESP, /*Spain*/
-    ITA, /*Italy*/
-    YEM, /*Yemen*/
-    CPV, /*Cape Verde*/
-    KAZ, /*Kazakhstan*/
-    KHM, /*Cambodia*/
-    CMR, /*Cameroon*/
-    CAN, /*Canada*/
-    QAT, /*Qatar*/
-    KEN, /*Kenya*/
-    CYP, /*Cyprus*/
-    KGZ, /*Kyrgyzstan*/
-    KIR, /*Kiribati*/
-    CHN, /*China*/
-    CCK, /*Cocos (Keeling) Islands*/
-    COL, /*Colombia*/
-    COM, /*Comoros*/
-    COG, /*Congo*/
-    COD, /*Congo, Democratic Republic of the*/
-    PRK, /*Korea, Democratic People's republic of*/
-    KOR, /*Korea, Republic of*/
-    CRI, /*Costa Rica*/
-    CIV, /*Cote d'Ivoire*/
-    CUB, /*Cuba*/
-    KWT, /*Kuwait*/
-    CUW, /*Curaçao*/
-    LAO, /*Lao People's Democratic Republic*/
-    LVA, /*Latvia*/
-    LSO, /*Lesotho*/
-    LBN, /*Lebanon*/
-    LBY, /*Libyan Arab Jamahiriya*/
-    LBR, /*Liberia*/
-    LIE, /*Liechtenstein*/
-    LTU, /*Lithuania*/
-    LUX, /*Luxembourg*/
-    MUS, /*Mauritius*/
-    MRT, /*Mauritania*/
-    MDG, /*Madagascar*/
-    MYT, /*Mayotte*/
-    MAC, /*Macao*/
-    MWI, /*Malawi*/
-    MYS, /*Malaysia*/
-    MLI, /*Mali*/
-    UMI, /*United States Minor Outlying Islands*/
-    MDV, /*Maldives*/
-    MLT, /*Malta*/
-    MAR, /*Morocco*/
-    MTQ, /*Martinique*/
-    MHL, /*Marshall Islands*/
-    MEX, /*Mexico*/
-    FSM, /*Micronesia, Federated States of*/
-    MOZ, /*Mozambique*/
-    MDA, /*Moldova*/
-    MCO, /*Monaco*/
-    MNG, /*Mongolia*/
-    MSR, /*Montserrat*/
-    MMR, /*Burma*/
-    NAM, /*Namibia*/
-    NRU, /*Nauru*/
-    NPL, /*Nepal*/
-    NER, /*Niger*/
-    NGA, /*Nigeria*/
-    NLD, /*Netherlands*/
-    NIC, /*Nicaragua*/
-    NIU, /*Niue*/
-    NZL, /*New Zealand*/
-    NCL, /*New Caledonia*/
-    NOR, /*Norway*/
-    ARE, /*United Arab Emirates*/
-    OMN, /*Oman*/
-    BVT, /*Bouvet Island*/
-    IMN, /*Isle of Man*/
-    NFK, /*Norfolk Island*/
-    CXR, /*Christmas Island*/
-    HMD, /*Heard Island and McDonald Islands*/
-    CYM, /*Cayman Islands*/
-    COK, /*Cook Islands*/
-    TCA, /*Turks and Caicos Islands*/
-    PAK, /*Pakistan*/
-    PLW, /*Palau*/
-    PSE, /*Palestinian Territory, Occupied*/
-    PAN, /*Panama*/
-    VAT, /*Holy See (Vatican City State)*/
-    PNG, /*Papua New Guinea*/
-    PRY, /*Paraguay*/
-    PER, /*Peru*/
-    PCN, /*Pitcairn*/
-    POL, /*Poland*/
-    PRT, /*Portugal*/
-    PRI, /*Puerto Rico*/
-    MKD, /*Macedonia, The Former Yugoslav Republic Of*/
-    REU, /*Reunion*/
-    RUS, /*Russian Federation*/
-    RWA, /*Rwanda*/
-    ROU, /*Romania*/
-    WSM, /*Samoa*/
-    SMR, /*San Marino*/
-    STP, /*Sao Tome and Principe*/
-    SAU, /*Saudi Arabia*/
-    SWZ, /*Swaziland*/
-    SHN, /*Saint Helena, Ascension And Tristan Da Cunha*/
-    MNP, /*Northern Mariana Islands*/
-    BLM, /*Saint Barthélemy*/
-    MAF, /*Saint Martin (French Part)*/
-    SEN, /*Senegal*/
-    VCT, /*Saint Vincent and the Grenadines*/
-    KNA, /*Saint Kitts and Nevis*/
-    LCA, /*Saint Lucia*/
-    SPM, /*Saint Pierre and Miquelon*/
-    SRB, /*Serbia*/
-    SYC, /*Seychelles*/
-    SGP, /*Singapore*/
-    SXM, /*Sint Maarten*/
-    SYR, /*Syrian Arab Republic*/
-    SVK, /*Slovakia*/
-    SVN, /*Slovenia*/
-    GBR, /*United Kingdom*/
-    USA, /*United States*/
-    SLB, /*Solomon Islands*/
-    SOM, /*Somalia*/
-    SDN, /*Sudan*/
-    SUR, /*Suriname*/
-    SLE, /*Sierra Leone*/
-    TJK, /*Tajikistan*/
-    THA, /*Thailand*/
-    TWN, /*Taiwan, Province of China*/
-    TZA, /*Tanzania, United Republic Of*/
-    TLS, /*Timor-Leste*/
-    TGO, /*Togo*/
-    TKL, /*Tokelau*/
-    TON, /*Tonga*/
-    TTO, /*Trinidad and Tobago*/
-    TUV, /*Tuvalu*/
-    TUN, /*Tunisia*/
-    TKM, /*Turkmenistan*/
-    TUR, /*Turkey*/
-    UGA, /*Uganda*/
-    UZB, /*Uzbekistan*/
-    UKR, /*Ukraine*/
-    WLF, /*Wallis and Futuna*/
-    URY, /*Uruguay*/
-    FRO, /*Faroe Islands*/
-    FJI, /*Fiji*/
-    PHL, /*Philippines*/
-    FIN, /*Finland*/
-    FLK, /*Falkland Islands (Malvinas)*/
-    FRA, /*France*/
-    GUF, /*French Guiana*/
-    PYF, /*French Polynesia*/
-    ATF, /*French Southern Territories*/
-    HRV, /*Croatia*/
-    CAF, /*Central African Republic*/
-    TCD, /*Chad*/
-    MNE, /*Montenegro*/
-    CZE, /*Czech Republic*/
-    CHL, /*Chile*/
-    CHE, /*Switzerland*/
-    SWE, /*Sweden*/
-    SJM, /*Svalbard and Jan Mayen*/
-    LKA, /*Sri Lanka*/
-    ECU, /*Ecuador*/
-    GNQ, /*Equatorial Guinea*/
-    ALA, /*Aland Islands*/
-    SLV, /*El Salvador*/
-    ERI, /*Eritrea*/
-    EST, /*Estonia*/
-    ETH, /*Ethiopia*/
-    ZAF, /*South Africa*/
-    SGS, /*South Georgia and the South Sandwich Islands*/
-    OST, /*South Ossetia*/
-    SSD, /*South Sudan*/
-    JAM, /*Jamaica*/
+    ABH /*Abkhazia*/
+    AUS /*Australia*/
+    AUT /*Austria*/
+    AZE /*Azerbaijan*/
+    ALB /*Albania*/
+    DZA /*Algeria*/
+    ASM /*American Samoa*/
+    AIA /*Anguilla*/
+    AGO /*Angola*/
+    AND /*Andorra*/
+    ATA /*Antarctica*/
+    ATG /*Antigua and Barbuda*/
+    ARG /*Argentina*/
+    ARM /*Armenia*/
+    ABW /*Aruba*/
+    AFG /*Afghanistan*/
+    BHS /*Bahamas*/
+    BGD /*Bangladesh*/
+    BRB /*Barbados*/
+    BHR /*Bahrain*/
+    BLR /*Belarus*/
+    BLZ /*Belize*/
+    BEL /*Belgium*/
+    BEN /*Benin*/
+    BMU /*Bermuda*/
+    BGR /*Bulgaria*/
+    BOL /*Bolivia, plurinational state of*/
+    BES /*Bonaire, Sint Eustatius and Saba*/
+    BIH /*Bosnia and Herzegovina*/
+    BWA /*Botswana*/
+    BRA /*Brazil*/
+    IOT /*British Indian Ocean Territory*/
+    BRN /*Brunei Darussalam*/
+    BFA /*Burkina Faso*/
+    BDI /*Burundi*/
+    BTN /*Bhutan*/
+    VUT /*Vanuatu*/
+    HUN /*Hungary*/
+    VEN /*Venezuela*/
+    VGB /*Virgin Islands, British*/
+    VIR /*Virgin Islands, U.S.*/
+    VNM /*Vietnam*/
+    GAB /*Gabon*/
+    HTI /*Haiti*/
+    GUY /*Guyana*/
+    GMB /*Gambia*/
+    GHA /*Ghana*/
+    GLP /*Guadeloupe*/
+    GTM /*Guatemala*/
+    GIN /*Guinea*/
+    GNB /*Guinea-Bissau*/
+    DEU /*Germany*/
+    GGY /*Guernsey*/
+    GIB /*Gibraltar*/
+    HND /*Honduras*/
+    HKG /*Hong Kong*/
+    GRD /*Grenada*/
+    GRL /*Greenland*/
+    GRC /*Greece*/
+    GEO /*Georgia*/
+    GUM /*Guam*/
+    DNK /*Denmark*/
+    JEY /*Jersey*/
+    DJI /*Djibouti*/
+    DMA /*Dominica*/
+    DOM /*Dominican Republic*/
+    EGY /*Egypt*/
+    ZMB /*Zambia*/
+    ESH /*Western Sahara*/
+    ZWE /*Zimbabwe*/
+    ISR /*Israel*/
+    IND /*India*/
+    IDN /*Indonesia*/
+    JOR /*Jordan*/
+    IRQ /*Iraq*/
+    IRN /*Iran, Islamic Republic of*/
+    IRL /*Ireland*/
+    ISL /*Iceland*/
+    ESP /*Spain*/
+    ITA /*Italy*/
+    YEM /*Yemen*/
+    CPV /*Cape Verde*/
+    KAZ /*Kazakhstan*/
+    KHM /*Cambodia*/
+    CMR /*Cameroon*/
+    CAN /*Canada*/
+    QAT /*Qatar*/
+    KEN /*Kenya*/
+    CYP /*Cyprus*/
+    KGZ /*Kyrgyzstan*/
+    KIR /*Kiribati*/
+    CHN /*China*/
+    CCK /*Cocos (Keeling) Islands*/
+    COL /*Colombia*/
+    COM /*Comoros*/
+    COG /*Congo*/
+    COD /*Congo, Democratic Republic of the*/
+    PRK /*Korea, Democratic People's republic of*/
+    KOR /*Korea, Republic of*/
+    CRI /*Costa Rica*/
+    CIV /*Cote d'Ivoire*/
+    CUB /*Cuba*/
+    KWT /*Kuwait*/
+    CUW /*Curaçao*/
+    LAO /*Lao People's Democratic Republic*/
+    LVA /*Latvia*/
+    LSO /*Lesotho*/
+    LBN /*Lebanon*/
+    LBY /*Libyan Arab Jamahiriya*/
+    LBR /*Liberia*/
+    LIE /*Liechtenstein*/
+    LTU /*Lithuania*/
+    LUX /*Luxembourg*/
+    MUS /*Mauritius*/
+    MRT /*Mauritania*/
+    MDG /*Madagascar*/
+    MYT /*Mayotte*/
+    MAC /*Macao*/
+    MWI /*Malawi*/
+    MYS /*Malaysia*/
+    MLI /*Mali*/
+    UMI /*United States Minor Outlying Islands*/
+    MDV /*Maldives*/
+    MLT /*Malta*/
+    MAR /*Morocco*/
+    MTQ /*Martinique*/
+    MHL /*Marshall Islands*/
+    MEX /*Mexico*/
+    FSM /*Micronesia, Federated States of*/
+    MOZ /*Mozambique*/
+    MDA /*Moldova*/
+    MCO /*Monaco*/
+    MNG /*Mongolia*/
+    MSR /*Montserrat*/
+    MMR /*Burma*/
+    NAM /*Namibia*/
+    NRU /*Nauru*/
+    NPL /*Nepal*/
+    NER /*Niger*/
+    NGA /*Nigeria*/
+    NLD /*Netherlands*/
+    NIC /*Nicaragua*/
+    NIU /*Niue*/
+    NZL /*New Zealand*/
+    NCL /*New Caledonia*/
+    NOR /*Norway*/
+    ARE /*United Arab Emirates*/
+    OMN /*Oman*/
+    BVT /*Bouvet Island*/
+    IMN /*Isle of Man*/
+    NFK /*Norfolk Island*/
+    CXR /*Christmas Island*/
+    HMD /*Heard Island and McDonald Islands*/
+    CYM /*Cayman Islands*/
+    COK /*Cook Islands*/
+    TCA /*Turks and Caicos Islands*/
+    PAK /*Pakistan*/
+    PLW /*Palau*/
+    PSE /*Palestinian Territory, Occupied*/
+    PAN /*Panama*/
+    VAT /*Holy See (Vatican City State)*/
+    PNG /*Papua New Guinea*/
+    PRY /*Paraguay*/
+    PER /*Peru*/
+    PCN /*Pitcairn*/
+    POL /*Poland*/
+    PRT /*Portugal*/
+    PRI /*Puerto Rico*/
+    MKD /*Macedonia, The Former Yugoslav Republic Of*/
+    REU /*Reunion*/
+    RUS /*Russian Federation*/
+    RWA /*Rwanda*/
+    ROU /*Romania*/
+    WSM /*Samoa*/
+    SMR /*San Marino*/
+    STP /*Sao Tome and Principe*/
+    SAU /*Saudi Arabia*/
+    SWZ /*Swaziland*/
+    SHN /*Saint Helena, Ascension And Tristan Da Cunha*/
+    MNP /*Northern Mariana Islands*/
+    BLM /*Saint Barthélemy*/
+    MAF /*Saint Martin (French Part)*/
+    SEN /*Senegal*/
+    VCT /*Saint Vincent and the Grenadines*/
+    KNA /*Saint Kitts and Nevis*/
+    LCA /*Saint Lucia*/
+    SPM /*Saint Pierre and Miquelon*/
+    SRB /*Serbia*/
+    SYC /*Seychelles*/
+    SGP /*Singapore*/
+    SXM /*Sint Maarten*/
+    SYR /*Syrian Arab Republic*/
+    SVK /*Slovakia*/
+    SVN /*Slovenia*/
+    GBR /*United Kingdom*/
+    USA /*United States*/
+    SLB /*Solomon Islands*/
+    SOM /*Somalia*/
+    SDN /*Sudan*/
+    SUR /*Suriname*/
+    SLE /*Sierra Leone*/
+    TJK /*Tajikistan*/
+    THA /*Thailand*/
+    TWN /*Taiwan, Province of China*/
+    TZA /*Tanzania, United Republic Of*/
+    TLS /*Timor-Leste*/
+    TGO /*Togo*/
+    TKL /*Tokelau*/
+    TON /*Tonga*/
+    TTO /*Trinidad and Tobago*/
+    TUV /*Tuvalu*/
+    TUN /*Tunisia*/
+    TKM /*Turkmenistan*/
+    TUR /*Turkey*/
+    UGA /*Uganda*/
+    UZB /*Uzbekistan*/
+    UKR /*Ukraine*/
+    WLF /*Wallis and Futuna*/
+    URY /*Uruguay*/
+    FRO /*Faroe Islands*/
+    FJI /*Fiji*/
+    PHL /*Philippines*/
+    FIN /*Finland*/
+    FLK /*Falkland Islands (Malvinas)*/
+    FRA /*France*/
+    GUF /*French Guiana*/
+    PYF /*French Polynesia*/
+    ATF /*French Southern Territories*/
+    HRV /*Croatia*/
+    CAF /*Central African Republic*/
+    TCD /*Chad*/
+    MNE /*Montenegro*/
+    CZE /*Czech Republic*/
+    CHL /*Chile*/
+    CHE /*Switzerland*/
+    SWE /*Sweden*/
+    SJM /*Svalbard and Jan Mayen*/
+    LKA /*Sri Lanka*/
+    ECU /*Ecuador*/
+    GNQ /*Equatorial Guinea*/
+    ALA /*Aland Islands*/
+    SLV /*El Salvador*/
+    ERI /*Eritrea*/
+    EST /*Estonia*/
+    ETH /*Ethiopia*/
+    ZAF /*South Africa*/
+    SGS /*South Georgia and the South Sandwich Islands*/
+    OST /*South Ossetia*/
+    SSD /*South Sudan*/
+    JAM /*Jamaica*/
     JPN /*Japan*/
 }
 
@@ -1411,7 +1411,7 @@ struct PaymentInstitution {
 }
 
 enum PaymentInstitutionRealm {
-    test,
+    test
     live
 }
 
