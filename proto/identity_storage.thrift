873e4f6f0d0cd37fb0c208127a8b1f0814aefa4a
Evgeny Levenets <levenets.e.e@gmail.com>
2018-06-06 16:22:22 +0300

    CDS-57: introduce identity storage service

---
+++ proto/identity_storage.thrift
@@ -0,0 +1,66 @@
+include "base.thrift"
+
+namespace java com.rbkmoney.damsel.identity_storage
+namespace erlang id_storage
+
+/**
+ * Интерфейс для безопасного хранения идентификационных данных
+ */
+
+typedef string IdentityDocumentToken
+
+union IdentityDocument {
+    1: RussianDomesticPassport russian_domestic_passport
+    2: RussianRetireeInsuranceCertificate russian_retiree_insurance_certificate
+}
+
+struct RussianDomesticPassport {
+    1: required string series
+    2: required string number
+    /* Наименование выдавшего паспорт органа */
+    3: required string issuer
+    /* Дата выдачи паспорта */
+    4: required base.Timestamp issued_at
+    /* Фамилия гражданина */
+    5: required string family_name
+    /* Имя гражданина */
+    6: required string first_name
+    /* Отчество гражданина */
+    7: optional string patronymic
+    /* Дата рождения гражданина */
+    8: required base.Timestamp birth_date
+    /* Место рождения гражданина */
+    9: required string birth_place
+}
+
+/* Страховое свидетельство обязательного пенсионного страхования */
+struct RussianRetireeInsuranceCertificate {
+    /* СНИЛС */
+    1: required string number
+}
+
+struct SafeIdentityDocumentData {
+    1: required IdentityDocumentToken token
+    2: required SafeIdentityDocument safe_identity_document
+}
+
+union SafeIdentityDocument {
+    1: SafeRussianDomesticPassport safe_russian_domestic_passport
+    2: SafeRussianRetireeInsuranceCertificate safe_russian_retiree_insurance_certificate
+}
+
+struct SafeRussianDomesticPassport {
+    1: required string series_masked
+    2: required string number_masked
+    3: required string fullname_masked
+}
+
+struct SafeRussianRetireeInsuranceCertificate {
+    1: required string number_masked
+}
+
+service IdentityStorage {
+
+    SafeIdentityDocumentData Put (1: IdentityDocument identity_document)
+    IdentityDocument Get (1: IdentityDocumentToken token)
+}
