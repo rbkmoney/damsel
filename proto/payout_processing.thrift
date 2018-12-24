661287215a37142aab9f7ac3591452fda36a8fb9
Pavel Popov <popoffpavel@yandex.ru>
2018-12-24 11:25:33 +0300

    Add amount in payout

--- proto/payout_processing.thrift
+++ proto/payout_processing.thrift
@@ -112,6 +112,7 @@ struct Payout {
     /* Время формирования платежного поручения, либо выплаты на карту  */
     4 : required base.Timestamp created_at
     5 : required PayoutStatus status
+    11: required domain.Cash amount
     6 : required domain.FinalCashFlow payout_flow
     7 : required PayoutType type
     8 : optional PayoutSummary summary
