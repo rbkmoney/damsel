d99319c1dbcfbec2f51e8e9835dee1fe391b4c2e
Roman Pushkov <pushkovroman@me.com>
2019-05-30 14:07:59 +0300

    HG-477: Allow provider fee selection by terminal (#447)
    * add terminal_is condition, remove terms from terminal
    
    * deprecate terms in Terminal instead of deleting
    
    * revert, make payment provision terms fields optional

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -1677,11 +1677,11 @@ struct WithdrawalProvider {
 }
 
 struct PaymentsProvisionTerms {
-    1: required CurrencySelector currencies
-    2: required CategorySelector categories
-    3: required PaymentMethodSelector payment_methods
-    6: required CashLimitSelector cash_limit
-    4: required CashFlowSelector cash_flow
+    1: optional CurrencySelector currencies
+    2: optional CategorySelector categories
+    3: optional PaymentMethodSelector payment_methods
+    6: optional CashLimitSelector cash_limit
+    4: optional CashFlowSelector cash_flow
     5: optional PaymentHoldsProvisionTerms holds
     7: optional PaymentRefundsProvisionTerms refunds
 }
