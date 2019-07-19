49d15e953a931c805f8775ffbc5d35ddb76a39aa
0x42 <bpozdnyakov@gmail.com>
2019-07-19 19:02:33 +0300

    add timeout file

---
+++ proto/timeout_behaviour.thrift
@@ -0,0 +1,12 @@
+namespace java com.rbkmoney.damsel.timeout_behaviour
+include "base.thrift"
+include "domain.thrift"
+
+ typedef base.Opaque Callback
+
+ union TimeoutBehaviour {
+    /** Неуспешное завершение взаимодействия с пояснением возникшей проблемы. */
+    1: domain.Failure failure
+    /** Вызов прокси для обработки события истечения таймаута. */
+    2: Callback callback
+}
