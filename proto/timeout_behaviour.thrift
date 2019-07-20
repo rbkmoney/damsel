30fe02661767e54ddad7a7627ad14495cae678ab
0x42 <bpozdnyakov@gmail.com>
2019-07-20 22:20:32 +0300

    add operation_timeout to TimeoutBehaviour

--- proto/timeout_behaviour.thrift
+++ proto/timeout_behaviour.thrift
@@ -6,8 +6,9 @@ include "domain.thrift"
 typedef base.Opaque Callback
 
 union TimeoutBehaviour {
+    1: domain.OperationTimeout operation_timeout
     /** Неуспешное завершение взаимодействия с пояснением возникшей проблемы. */
-    1: domain.Failure failure
+    2: domain.Failure failure
     /** Вызов прокси для обработки события истечения таймаута. */
-    2: Callback callback
+    3: Callback callback
 }
