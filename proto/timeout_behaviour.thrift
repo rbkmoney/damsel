99de5c542f582ce4c066a00bd967559574b88887
0x42 <bpozdnyakov@gmail.com>
2019-07-20 20:52:14 +0300

    delete whitespace

--- proto/timeout_behaviour.thrift
+++ proto/timeout_behaviour.thrift
@@ -2,9 +2,9 @@ namespace java com.rbkmoney.damsel.timeout_behaviour
 include "base.thrift"
 include "domain.thrift"
 
- typedef base.Opaque Callback
+typedef base.Opaque Callback
 
- union TimeoutBehaviour {
+union TimeoutBehaviour {
     /** Неуспешное завершение взаимодействия с пояснением возникшей проблемы. */
     1: domain.Failure failure
     /** Вызов прокси для обработки события истечения таймаута. */
