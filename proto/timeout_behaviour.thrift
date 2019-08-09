80e61dfcfc88734599a724131a5dbaebb20871af
0x42 <bpozdnyakov@gmail.com>
2019-08-09 10:18:37 +0300

    combine failures in OperationFailure

--- proto/timeout_behaviour.thrift
+++ proto/timeout_behaviour.thrift
@@ -6,9 +6,7 @@ include "domain.thrift"
 typedef base.Opaque Callback
 
 union TimeoutBehaviour {
-    1: domain.OperationTimeout operation_timeout
-    /** Неуспешное завершение взаимодействия с пояснением возникшей проблемы. */
-    2: domain.Failure failure
+    1: domain.OperationFailure operation_failure
     /** Вызов прокси для обработки события истечения таймаута. */
-    3: Callback callback
+    2: Callback callback
 }
