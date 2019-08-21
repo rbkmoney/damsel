9c2c9d91a34b30b3ad099564b63e7ebd94870a86
Roman Pushkov <pushkovroman@me.com>
2019-08-21 11:42:08 +0300

    fix weird deletions

--- proto/payment_processing.thrift
+++ proto/payment_processing.thrift
@@ -1349,6 +1349,7 @@ struct CustomerBinding {
     3: required DisposablePaymentResource payment_resource
     4: required CustomerBindingStatus     status
     5: optional PartyRevision             party_revision
+    6: optional domain.DataRevision       domain_revision
 }
 
 // Statuses
@@ -1488,6 +1489,7 @@ struct RecurrentPaymentToolParams {
     5: optional RecurrentPaymentToolID    id
     1: required PartyID                   party_id
     4: optional PartyRevision             party_revision
+    6: optional domain.DataRevision       domain_revision
     2: required ShopID                    shop_id
     3: required DisposablePaymentResource payment_resource
 }
