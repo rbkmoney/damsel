ca4087a7cf6c0b0d03a08bf3095c244dc18b766c
Sergey Yelin <elinsn@gmail.com>
2019-08-23 15:57:22 +0300

    Typo in comment

--- proto/accounter.thrift
+++ proto/accounter.thrift
@@ -1,6 +1,6 @@
 /**
 * NOTE: Данный протокол является устаревшим и может быть удалён в одном из будующих релизов
-* Для работы с маккаунтами используйте https://github.com/rbkmoney/shumpune-proto
+* Для работы с аккаунтами используйте https://github.com/rbkmoney/shumpune-proto
 **/
 
 include 'base.thrift'
