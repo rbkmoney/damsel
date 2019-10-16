507244af3ff6069bfe52aec3c40d43f9235ef0f9
Roman Pushkov <pushkovroman@me.com>
2019-10-16 12:03:27 +0300

    revert AdapterOptions to domain.ProxyOptions for better overall consistency

--- proto/p2p_adapter.thrift
+++ proto/p2p_adapter.thrift
@@ -20,8 +20,6 @@ typedef base.Opaque CallbackResponsePayload
 typedef base.Tag CallbackTag
 typedef base.ID UserInterationID
 
-typedef base.StringMap AdapterOptions
-
 /**
  * Требование адаптера к процессингу, отражающее дальнейший прогресс сессии взаимодействия
  * с третьей стороной.
@@ -158,9 +156,9 @@ struct Session {
  * Набор данных для взаимодействия с адаптером в рамках операции.
  */
 struct Context {
-    1: required Session        session
-    2: required OperationInfo  operation
-    3: optional AdapterOptions options = {}
+    1: required Session             session
+    2: required OperationInfo       operation
+    3: optional domain.ProxyOptions options = {}
 }
 
 /**
