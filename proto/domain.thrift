b917c1cc1f21a1025d69d08862fe52a5355b46f7
d.baykov <d.baykov@rbkmoney.com>
2019-10-17 15:53:07 +0300

    Add route info to InvoicePayment struct [2]

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -764,7 +764,6 @@ struct ContractAdjustment {
 //   ...
 
 struct TermSet {
-
     1: optional PaymentsServiceTerms payments
     2: optional RecurrentPaytoolsServiceTerms recurrent_paytools
     3: optional PayoutsServiceTerms payouts
@@ -774,10 +773,8 @@ struct TermSet {
 
 struct TimedTermSet {
     1: required base.TimestampInterval action_time
-
     2: required TermSet terms
 }
-
 
 struct TermSetHierarchy {
     3: optional string name
@@ -796,7 +793,6 @@ struct PaymentsServiceTerms {
     1: optional CurrencySelector currencies
     2: optional CategorySelector categories
     /* Invoice level*/
-
     4: optional PaymentMethodSelector payment_methods
     5: optional CashLimitSelector cash_limit
     /* Payment level */
@@ -1400,7 +1396,6 @@ struct PaymentMethodDefinition {
 
 union PaymentMethodSelector {
     1: list<PaymentMethodDecision> decisions
-
     2: set<PaymentMethodRef> value
 }
 
