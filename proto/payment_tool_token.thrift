89d9d46cba6531b7173d5abd423058da284b0482
0x42 <bpozdnyakov@gmail.com>
2019-11-29 12:19:48 +0300

    add another payment_tool

--- proto/payment_tool_token.thrift
+++ proto/payment_tool_token.thrift
@@ -10,8 +10,28 @@ namespace erlang ptt
 
 union PaymentToolToken {
     1: BankCardPayload bank_card_payload
+    2: PaymentTerminalPayload payment_terminal_payload
+    3: DigitalWalletPayload digital_wallet_payload
+    4: CryptoCurrencyPayload crypto_currency_payload
+    5: MobileCommercePayload mobile_commerce_payload
 }
 
 struct BankCardPayload {
     1: required domain.BankCard bank_card
+}
+
+struct PaymentTerminalPayload {
+    1: required domain.PaymentTerminal payment_terminal
+}
+
+struct DigitalWalletPayload {
+    1: required domain.DigitalWallet digital_wallet
+}
+
+struct CryptoCurrencyPayload {
+    1: required domain.CryptoCurrency crypto_currency
+}
+
+struct MobileCommercePayload {
+    1: required domain.MobileCommerce mobile_commerce
 }
