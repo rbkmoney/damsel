07d117ca6bad95b16409ae362aa6db4a8f70a426
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-05-21 13:55:23 +0300

    Reintroduce old PaymentMethods to ensure backwards compatibility

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -1480,11 +1480,14 @@ struct CashLimitDecision {
 /* Payment methods */
 
 union PaymentMethod {
+    1: BankCardPaymentSystem bank_card // Deprecated, use BankCardPaymentMethod instead
     2: TerminalPaymentProvider payment_terminal
     3: DigitalWalletProvider digital_wallet
+    4: TokenizedBankCard tokenized_bank_card // Deprecated, use BankCardPaymentMethod instead
+    5: BankCardPaymentSystem empty_cvv_bank_card // Deprecated, use BankCardPaymentMethod instead
     6: CryptoCurrency crypto_currency
     7: MobileOperator mobile
-    8: BankCardPaymentMethod bank_card
+    8: BankCardPaymentMethod new_bank_card
 }
 
 typedef BankCardTokenProvider BankCardProvider
