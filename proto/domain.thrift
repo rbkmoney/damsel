e95c428a834abab9e3379b8e33778e0a79fd06e5
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-05-21 15:15:36 +0300

    Replace is_tokenized with card_provider

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -1564,7 +1564,7 @@ struct BankCard {
     9: optional bool is_cvv_empty
    10: optional BankCardExpDate exp_date
    11: optional string cardholder_name
-   12: optional bool is_tokenized // TODO: come up with better solution
+   12: optional BankCardProvider card_provider
 }
 
 /** Дата экспирации */
