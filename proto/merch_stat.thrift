e90f9a9363235bf417623bda8aba9631b6b7cb3c
Pavel Popov <popoffpavel@yandex.ru>
2020-05-26 15:01:55 +0300

    Add external_id in merch_stat

--- proto/merch_stat.thrift
+++ proto/merch_stat.thrift
@@ -31,6 +31,7 @@ struct StatPayment {
     17: required domain.DataRevision domain_revision
     18: optional domain.InvoiceCart cart
     19: optional domain.AdditionalTransactionInfo additional_transaction_info
+    20: optional string external_id
 }
 
 union Payer {
@@ -223,6 +224,7 @@ struct StatInvoice {
     10: required string currency_symbolic_code
     11: optional base.Content context
     12: optional domain.InvoiceCart cart
+    13: optional string external_id
 }
 
 struct EnrichedStatInvoice {
@@ -350,6 +352,7 @@ struct StatRefund {
     10: required string currency_symbolic_code
     11: optional string reason
     12: optional domain.InvoiceCart cart
+    13: optional string external_id
 }
 
 union InvoicePaymentRefundStatus {
