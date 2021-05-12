d3e65fa53362e8615c9db3d931175e5d8466deae
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-05-12 16:43:36 +0300

    Update proto/domain.thrift
    Co-authored-by: Sergey Yelin <elinsn@gmail.com>

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -779,7 +779,7 @@ struct InternationalLegalEntity {
     /* Регистрационный номер */
     5: optional string registered_number
     /* Страна Мерчанта */
-    6: optional string country
+    6: optional Residence country
     /* Экономическая зона/блок Мерчанта: ЕЭЗ */
     7: optional string trade_bloc
 }
