a3c69d0088319f0791e3faf0e6d48e46a6386f8e
inallyoung <in.all.young@gmail.com>
2021-05-31 15:12:24 +0300

    Rename NKOInfo -> PaymentInstitutionAccount

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -894,10 +894,10 @@ union PayoutToolInfo {
     1: RussianBankAccount russian_bank_account
     2: InternationalBankAccount international_bank_account
     3: WalletInfo wallet_info
-    4: NKOInfo nko_info
+    4: PaymentInstitutionAccount nko_info
 }
 
-struct NKOInfo {
+struct PaymentInstitutionAccount {
     1: optional AccountID account_id
 }
 
