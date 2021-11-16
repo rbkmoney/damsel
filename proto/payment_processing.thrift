5953310c080ba5878fe8c6708796e3c0c5d04dfc
Sergey Yelin <elinsn@gmail.com>
2021-11-16 17:25:08 +0300

    ED-274: Add reference to ClaimManagement's Claim and relax requiremens for changeset

--- proto/payment_processing.thrift
+++ proto/payment_processing.thrift
@@ -2196,10 +2196,18 @@ typedef i32 ClaimRevision
 struct Claim {
     1: required ClaimID id
     2: required ClaimStatus status
-    3: required PartyChangeset changeset
+    3: optional PartyChangeset changeset
     4: required ClaimRevision revision
     5: required base.Timestamp created_at
     6: optional base.Timestamp updated_at
+    7: optional ClaimManagementClaimRef caused_by
+}
+
+// NOTE: Type for ClaimID and ClaimRevision must be in sync
+// with ClaimManagement's ClaimID and ClaimRevision
+struct ClaimManagementClaimRef {
+    1: required ClaimID id
+    2: required ClaimRevision revision
 }
 
 union ClaimStatus {
